eab8a101b71deb003d680b8be7f883df20e0564d
James Roper <james@jazzy.id.au>
2016-12-12 19:46:56 +1100

    Added consuming services Scala documentation
    Also added the circuit breaker metrics service for parity with the Java
    API, since it was not implemented.

---
+++ docs/manual/scala/guide/services/code/ServiceClients.scala
@@ -0,0 +1,105 @@
+package docs.scaladsl.services
+
+package implementhelloclient {
+
+  import helloservice.HelloService
+
+  import com.lightbend.lagom.scaladsl.server.{LagomApplication, LagomApplicationContext}
+  import play.api.libs.ws.ahc.AhcWSComponents
+
+  //#implement-hello-client
+  abstract class MyApplication(context: LagomApplicationContext)
+    extends LagomApplication(context)
+      with AhcWSComponents {
+
+    lazy val helloService = serviceClient.implement[HelloService]
+  }
+  //#implement-hello-client
+
+}
+
+package helloconsumer {
+
+  import akka.NotUsed
+  import com.lightbend.lagom.scaladsl.api.{Service, ServiceCall}
+  import helloservice.HelloService
+
+  import scala.concurrent.{ExecutionContext, Future}
+
+  trait MyService extends Service {
+    def sayHelloLagom: ServiceCall[NotUsed, String]
+
+    override def descriptor = {
+      import Service._
+
+      named("myservice").withCalls(call(sayHelloLagom))
+    }
+  }
+
+  //#hello-consumer
+  class MyServiceImpl(helloService: HelloService)
+    (implicit ec: ExecutionContext) extends MyService {
+
+    override def sayHelloLagom = ServiceCall { _ =>
+      val result: Future[String] =
+        helloService.sayHello.invoke("Lagom")
+
+      result.map { response =>
+        s"Hello service said: $response"
+      }
+    }
+  }
+  //#hello-consumer
+}
+
+package circuitbreakers {
+
+  import com.lightbend.lagom.scaladsl.api.{Descriptor, Service, ServiceCall}
+
+  trait HelloServiceWithCircuitBreaker extends Service {
+    def sayHi: ServiceCall[String, String]
+
+    def hiAgain: ServiceCall[String, String]
+
+    // @formatter:off
+    //#circuit-breakers
+    import com.lightbend.lagom.scaladsl.api.CircuitBreaker
+
+    def descriptor: Descriptor = {
+      import Service._
+
+      named("hello").withCalls(
+        namedCall("hi", this.sayHi),
+        namedCall("hiAgain", this.hiAgain)
+          .withCircuitBreaker(CircuitBreaker.identifiedBy("hello2"))
+      )
+    }
+    //#circuit-breakers
+    // @formatter:on
+  }
+
+}
+
+package metricsservice {
+
+  import helloservice._
+  import com.lightbend.lagom.scaladsl.server.{LagomApplication, LagomApplicationContext, LagomServer}
+  import play.api.libs.ws.ahc.AhcWSComponents
+  import com.softwaremill.macwire._
+
+  //#metrics-service
+  import com.lightbend.lagom.scaladsl.server.status.MetricsServiceComponents
+
+  abstract class MyApplication(context: LagomApplicationContext)
+    extends LagomApplication(context)
+      with AhcWSComponents
+      with MetricsServiceComponents {
+
+    lazy val lagomServer = LagomServer.forServices(
+      bindService[HelloService].to(wire[HelloServiceImpl]),
+      metricsServiceBinding
+    )
+  }
+  //#metrics-service
+
+}
---
+++ service/scaladsl/server/src/main/scala/com/lightbend/lagom/scaladsl/server/status/MetricsService.scala
@@ -0,0 +1,139 @@
+/*
+ * Copyright (C) 2016 Lightbend Inc. <http://www.lightbend.com>
+ */
+package com.lightbend.lagom.scaladsl.server.status
+
+import java.time.Instant
+
+import akka.NotUsed
+import akka.stream.scaladsl.Source
+import com.lightbend.lagom.internal.client.{ CircuitBreakerMetricsImpl, CircuitBreakerMetricsProviderImpl }
+import com.lightbend.lagom.internal.spi.CircuitBreakerMetricsProvider
+import com.lightbend.lagom.scaladsl.api.{ Service, ServiceCall }
+import com.lightbend.lagom.scaladsl.server.{ LagomServerComponents, LagomServiceBinder, LagomServiceBinding }
+import play.api.libs.json.{ Format, Json }
+
+import scala.collection.immutable
+import scala.collection.immutable.Seq
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.duration._
+import scala.util.control.NonFatal
+
+trait MetricsService extends Service {
+  /**
+   * Snapshot of current circuit breaker status
+   */
+  def currentCircuitBreakers: ServiceCall[NotUsed, immutable.Seq[CircuitBreakerStatus]]
+
+  /**
+   * Stream of circuit breaker status
+   */
+  def circuitBreakers: ServiceCall[NotUsed, Source[immutable.Seq[CircuitBreakerStatus], Any]]
+
+  override def descriptor = {
+    import Service._
+
+    named("metrics").withCalls(
+      pathCall("/_status/circuit-breaker/current", currentCircuitBreakers),
+      pathCall("/_status/circuit-breaker/stream", circuitBreakers)
+    ).withLocatableService(false)
+  }
+}
+
+/**
+ * Provides an in-built metrics service.
+ */
+trait MetricsServiceComponents extends LagomServerComponents {
+  def circuitBreakerMetricsProvider: CircuitBreakerMetricsProvider
+
+  lazy val metricsServiceBinding: LagomServiceBinding[MetricsService] = {
+    // Can't use the bindService macro here, since it's in the same compilation unit. The code below is exactly what
+    // the macro generates.
+    LagomServiceBinder(lagomServerBuilder, new MetricsService {
+      override def circuitBreakers: ServiceCall[NotUsed, Source[Seq[CircuitBreakerStatus], Any]] =
+        throw new NotImplementedError("Service methods and topics must not be invoked from service trait")
+      override def currentCircuitBreakers: ServiceCall[NotUsed, Seq[CircuitBreakerStatus]] =
+        throw new NotImplementedError("Service methods and topics must not be invoked from service trait")
+    }.descriptor).to(new MetricsServiceImpl(circuitBreakerMetricsProvider)(executionContext))
+  }
+}
+
+case class CircuitBreakerStatus(
+  id:                        String,
+  timestamp:                 Instant,
+  state:                     String,
+  totalSuccessCount:         Long,
+  totalFailureCount:         Long,
+  throughputOneMinute:       Double,
+  failedThroughputOneMinute: Double,
+  latencyMicros:             Latency
+)
+
+object CircuitBreakerStatus {
+  implicit val format: Format[CircuitBreakerStatus] = Json.format
+}
+
+case class Latency(
+  median:          Double,
+  percentile98th:  Double,
+  percentile99th:  Double,
+  percentile999th: Double,
+  mean:            Double,
+  min:             Long,
+  max:             Long
+)
+
+object Latency {
+  implicit val format: Format[Latency] = Json.format
+}
+
+private class MetricsServiceImpl(circuitBreakerMetricsProvider: CircuitBreakerMetricsProvider)(implicit ec: ExecutionContext) extends MetricsService {
+
+  override def currentCircuitBreakers = ServiceCall { _ =>
+    Future.successful(allCircuitBreakerStatus)
+  }
+
+  override def circuitBreakers = ServiceCall { _ =>
+    val source = Source.tick(100.milliseconds, 2.seconds, "tick").map { _ =>
+      allCircuitBreakerStatus
+    }
+    Future.successful(source)
+  }
+
+  private def allCircuitBreakerStatus: immutable.Seq[CircuitBreakerStatus] = {
+    import scala.collection.JavaConverters._
+    circuitBreakerMetricsProvider.asInstanceOf[CircuitBreakerMetricsProviderImpl].allMetrics().asScala.view
+      .flatMap { m =>
+        try {
+          Seq(circuitBreakerStatus(m))
+        } catch {
+          case NonFatal(e) =>
+            // might happen if the circuit breaker is removed, just ignore
+            Nil
+        }
+      }.to[immutable.Seq]
+  }
+
+  private def circuitBreakerStatus(m: CircuitBreakerMetricsImpl): CircuitBreakerStatus = {
+    val latencyHistogram = m.latency.getSnapshot
+    val latency = Latency(
+      median = latencyHistogram.getMedian,
+      percentile98th = latencyHistogram.get98thPercentile,
+      percentile99th = latencyHistogram.get99thPercentile,
+      percentile999th = latencyHistogram.get999thPercentile,
+      min = latencyHistogram.getMin,
+      max = latencyHistogram.getMax,
+      mean = latencyHistogram.getMean
+    )
+    CircuitBreakerStatus(
+      id = m.breakerId,
+      timestamp = Instant.now(),
+      state = m.state.getValue,
+      totalSuccessCount = m.successCount.getCount,
+      totalFailureCount = m.failureCount.getCount,
+      throughputOneMinute = m.throughput.getOneMinuteRate,
+      failedThroughputOneMinute = m.failureThroughput.getOneMinuteRate,
+      latencyMicros = latency
+    )
+  }
+}
