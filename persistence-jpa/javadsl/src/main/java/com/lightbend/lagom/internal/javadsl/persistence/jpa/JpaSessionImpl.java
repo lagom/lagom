faef7bb04bff8cd9c84a15d571f16aaeeac42780
Tim Moore <tim.moore@lightbend.com>
2017-01-03 13:55:48 +1100

    Use the Retry utility to create the JPA EntityManagerFactory

--- persistence-jpa/javadsl/src/main/java/com/lightbend/lagom/internal/javadsl/persistence/jpa/JpaSessionImpl.java
+++ persistence-jpa/javadsl/src/main/java/com/lightbend/lagom/internal/javadsl/persistence/jpa/JpaSessionImpl.java
@@ -14,7 +14,6 @@ import org.slf4j.LoggerFactory;
 import play.inject.ApplicationLifecycle;
 import scala.compat.java8.FutureConverters;
 import scala.compat.java8.JFunction0;
-import scala.concurrent.duration.Deadline;
 import scala.concurrent.duration.FiniteDuration;
 import slick.jdbc.JdbcBackend;
 
@@ -22,7 +21,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
@@ -30,30 +28,19 @@ import java.util.function.Function;
 @Singleton
 public class JpaSessionImpl implements JpaSession {
     // TODO: Make these constants configuration parameters in the Lagom framework implementation
-    private static final FiniteDuration INIT_TIMEOUT = FiniteDuration.create(1, TimeUnit.MINUTES);
-    private static final FiniteDuration INIT_RETRY_INTERVAL = FiniteDuration.create(5, TimeUnit.SECONDS);
     private static final String PERSISTENCE_UNIT_NAME = "default";
+    private static final FiniteDuration INIT_RETRY_INTERVAL = FiniteDuration.create(5, TimeUnit.SECONDS);
+    private static final double INIT_RETRY_FACTOR = 1.0;
+    private static final int INIT_RETRY_COUNT = 12;
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
-    private final ActorSystem actorSystem;
-    private final JdbcBackend.DatabaseDef slick;
+    private final JdbcBackend.DatabaseDef slickDb;
     private final CompletionStage<EntityManagerFactory> factoryCompletionStage;
 
     @Inject
     public JpaSessionImpl(ActorSystem actorSystem, SlickProvider slick, ApplicationLifecycle lifecycle) {
-        this.slick = slick.db();
-        this.actorSystem = actorSystem;
-        log.debug("Initializing JPA EntityManagerFactory with persistence unit name {}", PERSISTENCE_UNIT_NAME);
-        Deadline deadline = INIT_TIMEOUT.fromNow();
-        this.factoryCompletionStage =
-                tryCreateEntityManagerFactory(deadline, INIT_RETRY_INTERVAL)
-                        .whenComplete((entityManagerFactory, throwable) -> {
-                            if (entityManagerFactory != null) {
-                                log.debug("Completed initializing JPA EntityManagerFactory");
-                            } else {
-                                log.error("Could not initialize JPA EntityManagerFactory", throwable);
-                            }
-                        });
+        this.slickDb = slick.db();
+        this.factoryCompletionStage = createEntityManagerFactory(actorSystem);
         lifecycle.addStopHook(this::close);
     }
 
@@ -72,40 +59,26 @@ public class JpaSessionImpl implements JpaSession {
         });
     }
 
-    private CompletionStage<EntityManagerFactory> tryCreateEntityManagerFactory(Deadline deadline,
-                                                                                FiniteDuration retryInterval) {
-        CompletionStage<EntityManagerFactory> result = executeInSlickContext(() ->
-                Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME)
-        );
-        // This wrapping and then composing with identity logic is because
-        // CompletionStage has no equivalent to Scala's Future.recoverWith
-        return result.thenApply(CompletableFuture::completedFuture)
-                .exceptionally(e -> retryCreateEntityManagerFactory(e, deadline, retryInterval))
-                .thenCompose(Function.identity());
-    }
-
-    private CompletableFuture<EntityManagerFactory> retryCreateEntityManagerFactory(Throwable exception,
-                                                                                    Deadline deadline,
-                                                                                    FiniteDuration retryInterval) {
-        log.warn("Exception while initializing JPA EntityManagerFactory", exception);
-        CompletableFuture<EntityManagerFactory> future = new CompletableFuture<>();
-        if (deadline.timeLeft().lteq(retryInterval)) {
-            // Duration is up, don't try and recover from failure
-            future.completeExceptionally(exception);
-        } else {
-            log.info("Will retry initializing JPA EntityManagerFactory in {}", retryInterval);
-            actorSystem.scheduler().scheduleOnce(
-                    retryInterval,
-                    () -> tryCreateEntityManagerFactory(deadline, retryInterval)
-                            .thenAccept(future::complete)
-                            .exceptionally(ex -> {
-                                future.completeExceptionally(ex);
-                                return null;
-                            }),
-                    actorSystem.dispatcher()
-            );
-        }
-        return future;
+    private CompletionStage<EntityManagerFactory> createEntityManagerFactory(ActorSystem actorSystem) {
+        log.debug("Initializing JPA EntityManagerFactory with persistence unit name {}", PERSISTENCE_UNIT_NAME);
+        Retry jpaInitializer = new Retry(INIT_RETRY_INTERVAL, INIT_RETRY_FACTOR, INIT_RETRY_COUNT) {
+            @Override
+            public void onRetry(Throwable throwable, FiniteDuration delay, int remainingRetries) {
+                log.warn("Exception while initializing JPA EntityManagerFactory", throwable);
+                log.info("Will retry initializing JPA EntityManagerFactory {} times in {}", remainingRetries, delay);
+            }
+        };
+        return jpaInitializer.retry(
+                () -> Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME),
+                slickDb.ioExecutionContext(),
+                actorSystem.scheduler()
+        ).whenComplete((entityManagerFactory, throwable) -> {
+            if (entityManagerFactory != null) {
+                log.debug("Completed initializing JPA EntityManagerFactory");
+            } else {
+                log.error("Could not initialize JPA EntityManagerFactory", throwable);
+            }
+        });
     }
 
     private <T> CompletionStage<T> withEntityManager(Function<EntityManager, T> block) {
@@ -120,7 +93,7 @@ public class JpaSessionImpl implements JpaSession {
     }
 
     private <R> CompletionStage<R> executeInSlickContext(JFunction0<R> block) {
-        return FutureConverters.toJava(slick.io(block));
+        return FutureConverters.toJava(slickDb.io(block));
     }
 
     private CompletionStage<Done> close() {
