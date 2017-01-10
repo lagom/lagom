f8406996ee7ea3fe1db60b7ab452daca2cffe6cc
Ignasi Marimon-Clos <ignasi@lightbend.com>
2017-01-10 18:33:56 +0100

    Fixes #356

--- dev/service-registry/service-locator/src/main/scala/com/lightbend/lagom/discovery/ServiceRegistryActor.scala
+++ dev/service-registry/service-locator/src/main/scala/com/lightbend/lagom/discovery/ServiceRegistryActor.scala
@@ -3,19 +3,19 @@
  */
 package com.lightbend.lagom.discovery
 
-import java.net.{ InetSocketAddress, URI }
+import java.net.InetSocketAddress
 import java.util.regex.Pattern
 
 import akka.Done
 import akka.actor.{ Actor, Status }
+import com.google.inject.Inject
+import com.lightbend.lagom.internal.javadsl.registry.{ RegisteredService, ServiceRegistryService }
 import com.lightbend.lagom.javadsl.api.transport.{ TransportErrorCode, TransportException }
+import org.pcollections.{ PSequence, TreePVector }
+import play.api.Logger
 
 import scala.collection.JavaConverters._
 import scala.compat.java8.OptionConverters._
-import com.google.inject.Inject
-import com.lightbend.lagom.internal.javadsl.registry.{ RegisteredService, ServiceRegistryService }
-import org.pcollections.PSequence
-import org.pcollections.TreePVector
 
 object ServiceRegistryActor {
   case class Lookup(name: String)
@@ -31,10 +31,13 @@ object ServiceRegistryActor {
 
 class ServiceRegistryActor @Inject() (unmanagedServices: UnmanagedServices) extends Actor {
 
+  private val logger: Logger = Logger(classOf[ServiceLocatorServer])
+
   import ServiceRegistryActor._
 
   private var registry: Map[String, ServiceRegistryService] = unmanagedServices.services
   private var router = PartialFunction.empty[Route, InetSocketAddress]
+  private var routerFunctions = Seq.empty[PartialFunction[Route, InetSocketAddress]]
 
   override def receive: Receive = {
     case Lookup(name) => sender() ! registry.get(name).map(_.uri())
@@ -60,27 +63,38 @@ class ServiceRegistryActor @Inject() (unmanagedServices: UnmanagedServices) exte
       import scala.collection.JavaConverters._
       sender() ! RegisteredServices(TreePVector.from(services.asJava))
     case route: Route =>
+      routerFunctions.count(_.isDefinedAt(route)) match {
+        case 0 => logger.warn(s"Can't serve route: $route.")
+        case 1 => // expected case
+        case _ =>
+          val servers = routerFunctions
+            .filter(_.isDefinedAt(route)).map(_.apply(route)).mkString("[", ",", "]")
+          logger.warn(s"Ambiguous route resolution serving route: $route. Route served by $servers")
+
+      }
       sender() ! router.lift(route).fold[RouteResult](NotFound(registry))(Found.apply)
   }
 
   private def serviceRouter(service: ServiceRegistryService) = {
     val addressUri = service.uri
     val address = new InetSocketAddress(addressUri.getHost, addressUri.getPort)
-    service.acls.asScala.map {
+    val routerFunctions: Seq[PartialFunction[Route, InetSocketAddress]] = service.acls.asScala.map {
       case acl =>
         acl.method().asScala -> acl.pathRegex().asScala.map(Pattern.compile)
-    }.foldLeft(PartialFunction.empty[Route, InetSocketAddress]) {
-      case (function, (aclMethod, pathRegex)) =>
-        function.orElse {
+    }.map {
+      case (aclMethod, pathRegex) =>
+        val pf: PartialFunction[Route, InetSocketAddress] = {
           case Route(method, path) if aclMethod.forall(_.name == method) && pathRegex.forall(_.matcher(path).matches()) =>
             address
         }
+        pf
     }
+    routerFunctions
   }
 
   private def rebuildRouter() = {
-    router = registry.values.map(serviceRouter)
-      .foldLeft(PartialFunction.empty[Route, InetSocketAddress])(_ orElse _)
+    routerFunctions = registry.values.flatMap(serviceRouter).toSeq
+    router = routerFunctions.foldLeft(PartialFunction.empty[Route, InetSocketAddress])(_ orElse _)
   }
 
 }
