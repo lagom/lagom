5fb9813cc24dd6ddbc8cbe979cff12e1a7bb57f8
Ignasi Marimon-Clos <ignasi@lightbend.com>
2017-01-17 10:24:46 +0100

    Warn on premature close of stream

--- testkit/core/src/main/scala/com/lightbend/lagom/internal/testkit/TopicBufferActor.scala
+++ testkit/core/src/main/scala/com/lightbend/lagom/internal/testkit/TopicBufferActor.scala
@@ -3,7 +3,7 @@
  */
 package com.lightbend.lagom.internal.testkit
 
-import akka.actor.{ Actor, ActorRef, Props }
+import akka.actor.{ Actor, ActorLogging, ActorRef, Props, Terminated }
 
 import scala.collection.mutable
 
@@ -14,7 +14,7 @@ private[lagom] object TopicBufferActor {
 
 }
 
-private[lagom] class TopicBufferActor extends Actor {
+private[lagom] class TopicBufferActor extends Actor with ActorLogging {
 
   import TopicBufferActor._
 
@@ -25,7 +25,12 @@ private[lagom] class TopicBufferActor extends Actor {
     case SubscribeToBuffer(groupId, ref) => {
       downstreams = downstreams + (groupId -> ref)
       buffer.foreach(msg => ref.tell(msg, ActorRef.noSender))
+      context.watch(ref)
     }
+
+    case Terminated(deadWatch) =>
+      log.warning("Downstream actor {} terminated. This could mean that code consuming from the topic ended prematurely.", deadWatch)
+
     case message: AnyRef => {
       downstreams.values.foreach(ref => ref ! message)
       buffer append message
