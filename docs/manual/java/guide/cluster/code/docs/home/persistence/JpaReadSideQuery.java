eeb3e80907dbe1c9dc061a43990d4206da7292cf
Tim Moore <tim.moore@lightbend.com>
2017-02-13 13:36:55 +1100

    JPA read side documentation (#431)
    * Add JPA package to the "Persistence" javadoc group
    
    * Update the comment in persistence reference.conf
    
    It still referred to the now-deprecated CassandraReadSideProcessor.
    
    * Renamed RDBMS* example classes to Jdbc*
    
    This creates room for alternate RDBMS implementations (JPA for now,
    perhaps others later).
    
    * Add a JPA read-side example to the docs
    
    * Break down the dependency list
    
    This will help to avoid merge conflicts as projects are added.
    
    * Add JPA documentation
    
    * Fix the broken internal link

---
+++ docs/manual/java/guide/cluster/code/docs/home/persistence/JpaReadSideQuery.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2009-2017 Lightbend Inc. <https://www.lightbend.com>
+ */
+package docs.home.persistence;
+
+//#imports
+
+import akka.NotUsed;
+import com.lightbend.lagom.javadsl.api.ServiceCall;
+import com.lightbend.lagom.javadsl.persistence.jpa.JpaSession;
+import org.pcollections.PSequence;
+import org.pcollections.TreePVector;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+//#imports
+
+public interface JpaReadSideQuery {
+
+    interface BlogService {
+        public ServiceCall<NotUsed, PSequence<PostSummary>> getPostSummaries();
+    }
+
+    //#service-impl
+    public class BlogServiceImpl implements BlogService {
+
+        private final JpaSession jpaSession;
+
+        @Inject
+        public BlogServiceImpl(JpaSession jpaSession) {
+            this.jpaSession = jpaSession;
+        }
+
+        @Override
+        public ServiceCall<NotUsed, PSequence<PostSummary>> getPostSummaries() {
+            return request -> jpaSession
+                    .withTransaction(this::selectPostSummaries)
+                    .thenApply(TreePVector::from);
+        }
+
+        private List<PostSummary> selectPostSummaries(EntityManager entityManager) {
+            return entityManager
+                    .createQuery("SELECT" +
+                                    " NEW com.example.PostSummary(s.id, s.title)" +
+                                    " FROM BlogSummaryJpaEntity s",
+                            PostSummary.class
+                    )
+                    .getResultList();
+        }
+    }
+    //#service-impl
+}
