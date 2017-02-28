230e222a69fc2a824c489d86e51da36d1be53532
Tim Moore <tim.moore@lightbend.com>
2017-02-28 13:03:20 +1100

    Improve formatting
    The automatic scalariform reformatting of the previous code left
    something to be desired, aesthetically.

--- service/scaladsl/client/src/test/scala/com/lightbend/lagom/internal/scaladsl/client/ScaladslServiceResolverSpec.scala
+++ service/scaladsl/client/src/test/scala/com/lightbend/lagom/internal/scaladsl/client/ScaladslServiceResolverSpec.scala
@@ -9,9 +9,6 @@ import com.lightbend.lagom.scaladsl.api.{ CircuitBreaker, Descriptor, Service, S
 import com.lightbend.lagom.scaladsl.client.TestServiceClient
 import org.scalatest.{ FlatSpec, Matchers }
 
-/**
- *
- */
 class ScaladslServiceResolverSpec extends FlatSpec with Matchers {
 
   behavior of "ScaladslServiceResolver"
@@ -19,51 +16,63 @@ class ScaladslServiceResolverSpec extends FlatSpec with Matchers {
   it should "setup circuit-breakers for all method calls using default values when nothing is specified" in {
     assertCircuitBreaking(TestServiceClient.implement[Unspecified], CircuitBreaker.PerNode)
   }
+
   it should "setup circuit-breakers for all method calls using descriptor value when only descriptor's CB is specified" in {
     assertCircuitBreaking(TestServiceClient.implement[General], CircuitBreaker.identifiedBy("general-cb"))
   }
+
   it should "setup circuit-breakers with each specific CB when each call has a CB described" in {
     assertCircuitBreaking(TestServiceClient.implement[PerCall], CircuitBreaker.identifiedBy("one-cb"))
   }
 
   // --------------------------------------------------------------------------------------------
-  def assertCircuitBreaking(service: Service, expected: CircuitBreaker) = {
+  private def assertCircuitBreaking(service: Service, expected: CircuitBreaker) = {
     val resolved = new ScaladslServiceResolver(DefaultExceptionSerializer.Unresolved).resolve(service.descriptor)
     resolved.calls.head.circuitBreaker should be(Some(expected))
   }
 
   trait Unspecified extends Service {
+
     import Service._
+
     def one: ServiceCall[NotUsed, NotUsed]
+
     override def descriptor: Descriptor = {
-      named("Unspecified").
-        withCalls(
+      named("Unspecified")
+        .withCalls(
           namedCall("one", one)
         )
     }
   }
 
   trait General extends Service {
+
     import Service._
+
     def one: ServiceCall[NotUsed, NotUsed]
+
     override def descriptor: Descriptor = {
-      named(
-        "Unspecified"
-      ).withCalls(
+      named("Unspecified")
+        .withCalls(
           namedCall("one", one)
-        ).withCircuitBreaker(CircuitBreaker.identifiedBy("general-cb"))
+        )
+        .withCircuitBreaker(CircuitBreaker.identifiedBy("general-cb"))
     }
   }
 
   trait PerCall extends Service {
+
     import Service._
+
     def one: ServiceCall[NotUsed, NotUsed]
+
     override def descriptor: Descriptor = {
-      named(
-        "Unspecified"
-      ).withCalls(
-          namedCall("one", one).withCircuitBreaker(CircuitBreaker.identifiedBy("one-cb")) // overwrites default.
-        ).withCircuitBreaker(CircuitBreaker.identifiedBy("general-cb"))
+      named("Unspecified")
+        .withCalls(
+          namedCall("one", one)
+            .withCircuitBreaker(CircuitBreaker.identifiedBy("one-cb")) // overwrites default.
+        )
+        .withCircuitBreaker(CircuitBreaker.identifiedBy("general-cb"))
     }
   }
 
