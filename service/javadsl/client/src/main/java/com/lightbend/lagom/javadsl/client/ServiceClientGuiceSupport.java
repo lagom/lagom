4de3e16251852572c703babb1bfd3d9a386dfc3d
Ignasi Marimon-Clos <ignasi35@users.noreply.github.com>
2017-03-23 02:01:13 +0100

    Documents Service (Client) Guice Support (#611)
    * Documents Service (Client) Guice Support
    
    * Applies suggested fixes and some new rephrasing
    
    * Adds extra comment on wording
    
    * Fix syntax and typos
    
    * Adds class javadoc with use cases and detals wrt restrictions

--- service/javadsl/client/src/main/java/com/lightbend/lagom/javadsl/client/ServiceClientGuiceSupport.java
+++ service/javadsl/client/src/main/java/com/lightbend/lagom/javadsl/client/ServiceClientGuiceSupport.java
@@ -9,8 +9,20 @@ import com.lightbend.lagom.javadsl.api.ServiceInfo;
 
 import javax.inject.Singleton;
 
+/**
+ * Applications that already use Guice as their DI framework may implement this interface to bind clients for Lagom
+ * services.
+ */
 public interface ServiceClientGuiceSupport {
 
+    /**
+     * Binds a client for <code>clientInterface</code> which Guice's Injector makes available
+     * so client classes may declare the dependency on their <code>@Inject</code> annotated constructor.
+     * <p>
+     * Applications that want to consume Lagom services must provide a {@link ServiceInfo}. Using
+     * {@link ServiceClientGuiceSupport#bindClient(Class)} requires providing a {@link ServiceInfo} via
+     * {@link ServiceClientGuiceSupport#bindServiceInfo(ServiceInfo)}.
+     */
     default <T> void bindClient(Class<T> clientInterface) {
         BinderAccessor.binder(this).bind(clientInterface)
                 .toProvider(new ServiceClientProvider<T>(clientInterface))
@@ -18,7 +30,11 @@ public interface ServiceClientGuiceSupport {
     }
 
     /**
-     * Provides the ServiceInfo to use Lagom service clients.
+     * Registers a {@link ServiceInfo} for this application. This step is required to interact with Lagom services.
+     * <p>
+     * This method must be invoked exactly once.
+     *
+     * @param serviceInfo the metadata identifying this Lagom Service.
      */
     default void bindServiceInfo(ServiceInfo serviceInfo) {
         BinderAccessor.binder(this).bind(ServiceInfo.class)
