eefb7689308b4ee2964438073779717792b71f50
Tim Moore <tim.moore@lightbend.com>
2017-03-30 18:46:55 +1100

    Warn on implicit keyspace config (#632)
    * Warn on implicit keyspace config
    
    This lets users know that relying on the default is deprecated, and that
    they should configure keyspaces explicitly in application.conf.
    
    See #578
    
    * Fix test failure
    
    * Implement review suggestions and simplify
    
    * Print a more verbose and explanatory warning

---
+++ persistence-cassandra/core/src/main/scala/com/lightbend/lagom/internal/persistence/cassandra/CassandraKeyspaceConfig.scala
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2016-2017 Lightbend Inc. <https://www.lightbend.com>
+ */
+package com.lightbend.lagom.internal.persistence.cassandra
+
+import akka.event.LoggingAdapter
+import com.typesafe.config.Config
+
+private[lagom] object CassandraKeyspaceConfig {
+
+  def validateKeyspace(namespace: String, defaultNamespace: String, config: Config, log: LoggingAdapter): Unit = {
+    if (log.isWarningEnabled) {
+      val keyspace = config.getString(s"$namespace.keyspace")
+      val defaultPath = s"$defaultNamespace.keyspace"
+      if (config.hasPath(defaultPath) && keyspace == config.getString(defaultPath)) {
+        log.warning(
+          "Configuration for [{}] uses deprecated default value [{}]. " +
+            "Please set an explicit keyspace value in application.conf if you haven't already. " +
+            "The default will be removed in a future version of Lagom.",
+          s"$namespace.keyspace",
+          keyspace
+        )
+      }
+    }
+  }
+
+}
--- persistence-cassandra/scaladsl/src/main/scala/com/lightbend/lagom/internal/scaladsl/persistence/cassandra/CassandraPersistentEntityRegistry.scala
+++ persistence-cassandra/scaladsl/src/main/scala/com/lightbend/lagom/internal/scaladsl/persistence/cassandra/CassandraPersistentEntityRegistry.scala
@@ -4,9 +4,11 @@
 package com.lightbend.lagom.internal.scaladsl.persistence.cassandra
 
 import akka.actor.ActorSystem
+import akka.event.Logging
 import akka.persistence.cassandra.query.scaladsl.CassandraReadJournal
 import akka.persistence.query.PersistenceQuery
 import akka.persistence.query.scaladsl.EventsByTagQuery2
+import com.lightbend.lagom.internal.persistence.cassandra.CassandraKeyspaceConfig
 import com.lightbend.lagom.internal.scaladsl.persistence.AbstractPersistentEntityRegistry
 
 /**
@@ -14,6 +16,21 @@ import com.lightbend.lagom.internal.scaladsl.persistence.AbstractPersistentEntit
  */
 private[lagom] final class CassandraPersistentEntityRegistry(system: ActorSystem)
   extends AbstractPersistentEntityRegistry(system) {
+
+  private val log = Logging.getLogger(system, getClass)
+
+  CassandraKeyspaceConfig.validateKeyspace(
+    namespace = "cassandra-journal",
+    defaultNamespace = "cassandra-journal.defaults",
+    system.settings.config,
+    log
+  )
+  CassandraKeyspaceConfig.validateKeyspace(
+    namespace = "cassandra-snapshot-store",
+    defaultNamespace = "cassandra-snapshot-store.defaults",
+    system.settings.config,
+    log
+  )
 
   override protected val journalId = CassandraReadJournal.Identifier
 
