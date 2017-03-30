782eb7dbf1d672b848da7455e0ae9ae17683f7e1
Tim Moore <tim.moore@lightbend.com>
2017-03-30 18:46:55 +1100

    Warn on implicit keyspace config (#632)
    * Warn on implicit keyspace config
    
    This lets users know that relying on the default is deprecated, and that
    they should configure keyspaces explicitly in application.conf.
    
    See #578
    
    * Fix test failure
    
    * Implement review suggestions and simplify
    
    * Print a more verbose and explanatory warning

--- persistence-cassandra/javadsl/src/main/scala/com/lightbend/lagom/internal/javadsl/persistence/cassandra/CassandraPersistentEntityRegistry.scala
+++ persistence-cassandra/javadsl/src/main/scala/com/lightbend/lagom/internal/javadsl/persistence/cassandra/CassandraPersistentEntityRegistry.scala
@@ -6,11 +6,13 @@ package com.lightbend.lagom.internal.javadsl.persistence.cassandra
 import javax.inject.{ Inject, Singleton }
 
 import akka.actor.ActorSystem
+import akka.event.Logging
 import akka.persistence.cassandra.query.scaladsl.CassandraReadJournal
 import akka.persistence.query.PersistenceQuery
 import akka.persistence.query.scaladsl.EventsByTagQuery2
 import com.google.inject.Injector
 import com.lightbend.lagom.internal.javadsl.persistence.AbstractPersistentEntityRegistry
+import com.lightbend.lagom.internal.persistence.cassandra.CassandraKeyspaceConfig
 
 /**
  * Internal API
@@ -18,6 +20,21 @@ import com.lightbend.lagom.internal.javadsl.persistence.AbstractPersistentEntity
 @Singleton
 private[lagom] final class CassandraPersistentEntityRegistry @Inject() (system: ActorSystem, injector: Injector)
   extends AbstractPersistentEntityRegistry(system, injector) {
+
+  private val log = Logging.getLogger(system, getClass)
+
+  CassandraKeyspaceConfig.validateKeyspace(
+    namespace = "cassandra-journal",
+    defaultNamespace = "cassandra-journal.defaults",
+    system.settings.config,
+    log
+  )
+  CassandraKeyspaceConfig.validateKeyspace(
+    namespace = "cassandra-snapshot-store",
+    defaultNamespace = "cassandra-snapshot-store.defaults",
+    system.settings.config,
+    log
+  )
 
   override protected val journalId = CassandraReadJournal.Identifier
 
