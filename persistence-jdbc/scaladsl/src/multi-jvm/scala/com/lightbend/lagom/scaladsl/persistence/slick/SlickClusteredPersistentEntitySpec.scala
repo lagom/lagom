74bdd7cf23e4ead8c767a98c9d010a3850ebf61f
Pawel <odwrotnie@gmail.com>
2017-05-05 04:31:00 +0200

    Slick read side (#714)
    * Slick read side
    
    * Slick instead of JDBC in docs
    
    * Explicitly passing ()
    
    * #714 fixes

---
+++ persistence-jdbc/scaladsl/src/multi-jvm/scala/com/lightbend/lagom/scaladsl/persistence/slick/SlickClusteredPersistentEntitySpec.scala
@@ -0,0 +1,74 @@
+package com.lightbend.lagom.scaladsl.persistence.slick
+
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, Materializer}
+import com.lightbend.lagom.scaladsl.persistence.TestEntity.Evt
+import com.lightbend.lagom.scaladsl.persistence.multinode.{AbstractClusteredPersistentEntityConfig, AbstractClusteredPersistentEntitySpec}
+import com.lightbend.lagom.scaladsl.persistence.{ReadSideProcessor, TestEntitySerializerRegistry}
+import com.lightbend.lagom.scaladsl.playjson.JsonSerializerRegistry
+import com.typesafe.config.{Config, ConfigFactory}
+import org.h2.tools.Server
+import play.api.{Configuration, Environment}
+import play.api.db.HikariCPComponents
+import play.api.inject.{ApplicationLifecycle, DefaultApplicationLifecycle}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+object SlickClusteredPersistentEntityConfig extends AbstractClusteredPersistentEntityConfig {
+  override def additionalCommonConfig(databasePort: Int): Config = ConfigFactory.parseString(
+    s"""
+      db.default.driver=org.h2.Driver
+      db.default.url="jdbc:h2:tcp://localhost:$databasePort/mem:JdbcClusteredPersistentEntitySpec"
+    """)
+}
+
+class SlickClusteredPersistentEntitySpecMultiJvmNode1 extends SlickClusteredPersistentEntitySpec
+class SlickClusteredPersistentEntitySpecMultiJvmNode2 extends SlickClusteredPersistentEntitySpec
+class SlickClusteredPersistentEntitySpecMultiJvmNode3 extends SlickClusteredPersistentEntitySpec
+
+class SlickClusteredPersistentEntitySpec
+  extends AbstractClusteredPersistentEntitySpec(SlickClusteredPersistentEntityConfig) {
+
+  import SlickClusteredPersistentEntityConfig._
+
+  var h2: Server = _
+
+  override protected def atStartup(): Unit = {
+    runOn(node1) {
+      h2 = Server.createTcpServer("-tcpPort", databasePort.toString).start()
+    }
+    enterBarrier("h2-started")
+    super.atStartup()
+  }
+
+  override protected def afterTermination(): Unit = {
+    super.afterTermination()
+    defaultApplicationLifecycle.stop()
+    Option(h2).foreach(_.stop())
+  }
+
+  lazy val defaultApplicationLifecycle = new DefaultApplicationLifecycle
+
+  override lazy val components: SlickPersistenceComponents =
+    new SlickPersistenceComponents with HikariCPComponents {
+      override def actorSystem: ActorSystem = SlickClusteredPersistentEntitySpec.this.system
+      override def executionContext: ExecutionContext = system.dispatcher
+      override lazy val materializer: Materializer = ActorMaterializer.create(system)
+      override lazy val configuration: Configuration = Configuration(system.settings.config)
+      override def environment: Environment = SlickClusteredPersistentEntityConfig.environment
+      override lazy val applicationLifecycle: ApplicationLifecycle = defaultApplicationLifecycle
+      override def jsonSerializerRegistry: JsonSerializerRegistry = TestEntitySerializerRegistry
+    }
+
+  lazy val jdbcTestEntityReadSide: SlickTestEntityReadSide =
+    new SlickTestEntityReadSide(
+      components.db, components.profile)(components.executionContext)
+
+  override protected def getAppendCount(id: String): Future[Long] =
+    jdbcTestEntityReadSide.getAppendCount(id)
+
+  override protected def readSideProcessor: () => ReadSideProcessor[Evt] = {
+    () => new SlickTestEntityReadSide.TestEntityReadSideProcessor(
+      components.slickReadSide, components.db, components.profile)(components.executionContext)
+  }
+}
---
+++ persistence-jdbc/scaladsl/src/test/scala/com/lightbend/lagom/scaladsl/persistence/slick/SlickPersistenceSpec.scala
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2016-2017 Lightbend Inc. <https://www.lightbend.com>
+ */
+package com.lightbend.lagom.scaladsl.persistence.slick
+
+import akka.actor.setup.ActorSystemSetup
+import akka.actor.{ ActorSystem, BootstrapSetup }
+import akka.cluster.Cluster
+import com.lightbend.lagom.internal.persistence.ReadSideConfig
+import com.lightbend.lagom.internal.persistence.jdbc.{ SlickOffsetStore, SlickProvider }
+import com.lightbend.lagom.internal.scaladsl.persistence.jdbc.OffsetTableConfiguration
+import com.lightbend.lagom.internal.scaladsl.persistence.slick.SlickReadSideImpl
+import com.lightbend.lagom.persistence.{ ActorSystemSpec, PersistenceSpec }
+import com.lightbend.lagom.scaladsl.persistence.jdbc.testkit.TestUtil
+import com.lightbend.lagom.scaladsl.playjson.JsonSerializerRegistry
+import com.typesafe.config.{ Config, ConfigFactory }
+import play.api.db.{ Database, Databases }
+import play.api.{ Configuration, Environment }
+
+import scala.concurrent.{ Await, ExecutionContext }
+import scala.concurrent.duration._
+import scala.util.Random
+
+abstract class SlickPersistenceSpec private (_system: ActorSystem)(implicit ec: ExecutionContext)
+  extends ActorSystemSpec(_system) {
+
+  def this(testName: String, config: Config, registry: JsonSerializerRegistry)(implicit ec: ExecutionContext) =
+    this(ActorSystem(testName, ActorSystemSetup(
+      BootstrapSetup(
+        config.withFallback(TestUtil.clusterConfig()).withFallback(Configuration.load(Environment.simple()).underlying)
+      ),
+      JsonSerializerRegistry.serializationSetupFor(registry)
+    )))
+
+  def this(config: Config, registry: JsonSerializerRegistry)(implicit ec: ExecutionContext) =
+    this(PersistenceSpec.getCallerName(getClass), config, registry)
+
+  def this(registry: JsonSerializerRegistry)(implicit ec: ExecutionContext) =
+    this(ConfigFactory.empty(), registry)
+
+  // late initialization of database
+  private var _database: Option[Database] = None
+  protected def database = _database match {
+    case Some(db) => db
+    case None =>
+      val dbName = s"${system.name}_${Random.alphanumeric.take(8).mkString}"
+
+      val db = Databases.inMemory(dbName, config = Map("jndiName" -> "DefaultDS"))
+      _database = Some(db)
+      db
+  }
+  protected lazy val slick = new SlickProvider(system, null)
+  protected lazy val slickReadSide: SlickReadSide = new SlickReadSideImpl(
+    slick,
+    new SlickOffsetStore(
+      system,
+      slick,
+      new OffsetTableConfiguration(Configuration(system.settings.config), ReadSideConfig())
+    )
+  )
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+
+    // Join ourselves - needed because we're using cluster singleton to create tables
+    val cluster = Cluster(system)
+    cluster.join(cluster.selfAddress)
+
+    // Trigger database to be loaded and registered to JNDI
+    database.dataSource
+
+    // Trigger tables to be created
+    Await.ready(slick.ensureTablesCreated(), 20.seconds)
+
+    TestUtil.awaitPersistenceInit(system)
+  }
+
+  override def afterAll(): Unit = {
+    _database.foreach(_.shutdown())
+    super.afterAll()
+  }
+}
