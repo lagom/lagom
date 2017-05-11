b85799329ddb08bada22c117e5d25480db653d8f
vshin <vladimir.shin@gmail.com>
2017-05-12 00:16:59 +0900

    - Create com.lightbend.lagom.internal.persistence.cassandra.CassandraProvider class to separate read-side cassandra from global read-side settings - Add possibility to disable "offset store" table auto creation - Remove obsolete logic - Update documentation - Warning fixes

---
+++ persistence-cassandra/core/src/main/scala/com/lightbend/lagom/internal/persistence/cassandra/CassandraProvider.scala
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2016-2017 Lightbend Inc. <https://www.lightbend.com>
+ */
+package com.lightbend.lagom.internal.persistence.cassandra
+
+import akka.actor.ActorSystem
+
+/**
+ * Internal API
+ */
+private[lagom] class CassandraProvider(system: ActorSystem) {
+  private val cassandraConfig = system.settings.config.getConfig("lagom.persistence.read-side.cassandra")
+
+  val autoCreateTables: Boolean = cassandraConfig.getBoolean("tables-autocreate")
+}
