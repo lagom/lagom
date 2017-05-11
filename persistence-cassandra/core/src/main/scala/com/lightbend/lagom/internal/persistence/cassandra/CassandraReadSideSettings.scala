c2fc12fa8dc81d316e926fe9c555fc0c60cc8043
vshin <vladimir.shin@gmail.com>
2017-05-12 03:13:01 +0900

    - Add @Inject annotation

--- persistence-cassandra/core/src/main/scala/com/lightbend/lagom/internal/persistence/cassandra/CassandraReadSideSettings.scala
+++ persistence-cassandra/core/src/main/scala/com/lightbend/lagom/internal/persistence/cassandra/CassandraReadSideSettings.scala
@@ -3,12 +3,13 @@
  */
 package com.lightbend.lagom.internal.persistence.cassandra
 
+import javax.inject.Inject
 import akka.actor.ActorSystem
 
 /**
  * Internal API
  */
-private[lagom] class CassandraReadSideSettings(system: ActorSystem) {
+private[lagom] class CassandraReadSideSettings @Inject() (system: ActorSystem) {
   private val cassandraConfig = system.settings.config.getConfig("lagom.persistence.read-side.cassandra")
 
   val autoCreateTables: Boolean = cassandraConfig.getBoolean("tables-autocreate")
