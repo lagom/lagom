b3fd634f9da7190ef8f016a4a68490cf3d45a61d
Renato  Cavalcanti <renato@strongtyped.io>
2017-05-29 19:17:27 +0200

    align maven archetype with lagom-java.g8

--- dev/archetypes/maven-java/src/main/resources/archetype-resources/__service1Name__-impl/src/main/java/__packageInPathFormat__/__service1Name__/impl/__service1ClassName__Event.java
+++ dev/archetypes/maven-java/src/main/resources/archetype-resources/__service1Name__-impl/src/main/java/__packageInPathFormat__/__service1Name__/impl/__service1ClassName__Event.java
@@ -3,7 +3,10 @@
  */
 package ${package}.${service1Name}.impl;
 
-import javax.annotation.Nullable;
+import com.lightbend.lagom.javadsl.persistence.AggregateEvent;
+import com.lightbend.lagom.javadsl.persistence.AggregateEventShards;
+import com.lightbend.lagom.javadsl.persistence.AggregateEventTag;
+import com.lightbend.lagom.javadsl.persistence.AggregateEventTagger;
 import lombok.Value;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
@@ -18,7 +21,15 @@ import com.lightbend.lagom.serialization.Jsonable;
  * By convention, the events should be inner classes of the interface, which
  * makes it simple to get a complete picture of what events an entity has.
  */
-public interface ${service1ClassName}Event extends Jsonable {
+public interface ${service1ClassName}Event extends Jsonable, AggregateEvent<${service1ClassName}Event> {
+
+  /**
+   * Tags are used for getting and publishing streams of events. Each event
+   * will have this tag, and in this case, we are partitioning the tags into
+   * 4 shards, which means we can have 4 concurrent processors/publishers of
+   * events.
+   */
+  AggregateEventShards<${service1ClassName}Event> TAG = AggregateEventTag.sharded(${service1ClassName}Event.class, 4);
 
   /**
    * An event that represents a change in greeting message.
@@ -27,11 +38,21 @@ public interface ${service1ClassName}Event extends Jsonable {
   @Value
   @JsonDeserialize
   public final class GreetingMessageChanged implements ${service1ClassName}Event {
+
+    public final String name;
     public final String message;
 
     @JsonCreator
-    public GreetingMessageChanged(String message) {
+    public GreetingMessageChanged(String name, String message) {
+      this.name = Preconditions.checkNotNull(name, "name");
       this.message = Preconditions.checkNotNull(message, "message");
     }
   }
+
+
+  @Override
+  default AggregateEventTagger<${service1ClassName}Event> aggregateTag() {
+    return TAG;
+  }
+
 }
