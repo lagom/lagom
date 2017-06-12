3349c2b7d4fd51c442acfc21c374cd77ac788646
Ignasi Marimon-Clos <ignasi@lightbend.com>
2017-06-12 15:54:25 +0200

    Bumps akka to 2.4.19 as suggested by Akka team

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
 
   // Version numbers
   val PlayVersion = "2.5.13"
-  val AkkaVersion = "2.4.17"
+  val AkkaVersion = "2.4.19"
   val ScalaVersion = "2.11.11"
   val AkkaPersistenceCassandraVersion = "0.26"
   val ScalaTestVersion = "3.0.1"
