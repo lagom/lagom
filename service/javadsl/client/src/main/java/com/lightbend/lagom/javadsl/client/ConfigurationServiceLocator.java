f6afe6f367cee50196cc5fe22e98dbaee047ca6e
Tim Moore <tim.moore@lightbend.com>
2017-07-27 15:32:20 +0930

    Reindent ConfigurationServiceLocator

--- service/javadsl/client/src/main/java/com/lightbend/lagom/javadsl/client/ConfigurationServiceLocator.java
+++ service/javadsl/client/src/main/java/com/lightbend/lagom/javadsl/client/ConfigurationServiceLocator.java
@@ -28,47 +28,47 @@ import java.util.concurrent.CompletionStage;
 @Singleton
 public class ConfigurationServiceLocator extends CircuitBreakingServiceLocator {
 
-  private static final String LAGOM_SERVICES_KEY = "lagom.services";
-  private final PMap<String, URI> services;
+    private static final String LAGOM_SERVICES_KEY = "lagom.services";
+    private final PMap<String, URI> services;
 
 
-  /**
-   * @deprecated Use constructor accepting {@link CircuitBreakersPanel} instead
-   * @param circuitBreakers
-   */
-  @Deprecated
-  public ConfigurationServiceLocator(Configuration configuration, CircuitBreakers circuitBreakers) {
-    this(configuration.underlying(), new CircuitBreakersPanelImpl(circuitBreakers));
-  }
-
-  @Inject
-  public ConfigurationServiceLocator(Config config, CircuitBreakersPanel circuitBreakersPanel) {
-    super(circuitBreakersPanel);
-
-    Map<String, URI> services = new HashMap<>();
-
-    if (config.hasPath(LAGOM_SERVICES_KEY)) {
-      Config configServices = config.getConfig(LAGOM_SERVICES_KEY);
-      for (String key: configServices.root().keySet()) {
-        try {
-          String value = configServices.getString(key);
-          URI uri = new URI(value);
-          services.put(key, uri);
-        } catch (ConfigException.WrongType e) {
-          throw new IllegalStateException("Error loading configuration for " + getClass().getSimpleName() + ". Expected lagom.services." + key + " to be a String, but was " + configServices.getValue(key).valueType(), e);
-        } catch (URISyntaxException e) {
-          throw new IllegalStateException("Error loading configuration for  " + getClass().getSimpleName() + ". Expected lagom.services." + key + " to be a URI, but it failed to parse", e);
-        }
-      }
+    /**
+     * @deprecated Use constructor accepting {@link CircuitBreakersPanel} instead
+     * @param circuitBreakers
+     */
+    @Deprecated
+    public ConfigurationServiceLocator(Configuration configuration, CircuitBreakers circuitBreakers) {
+        this(configuration.underlying(), new CircuitBreakersPanelImpl(circuitBreakers));
     }
-    this.services = HashTreePMap.from(services);
-  }
+
+    @Inject
+    public ConfigurationServiceLocator(Config config, CircuitBreakersPanel circuitBreakersPanel) {
+        super(circuitBreakersPanel);
+
+        Map<String, URI> services = new HashMap<>();
+
+        if (config.hasPath(LAGOM_SERVICES_KEY)) {
+            Config configServices = config.getConfig(LAGOM_SERVICES_KEY);
+            for (String key: configServices.root().keySet()) {
+                try {
+                    String value = configServices.getString(key);
+                    URI uri = new URI(value);
+                    services.put(key, uri);
+                } catch (ConfigException.WrongType e) {
+                    throw new IllegalStateException("Error loading configuration for " + getClass().getSimpleName() + ". Expected lagom.services." + key + " to be a String, but was " + configServices.getValue(key).valueType(), e);
+                } catch (URISyntaxException e) {
+                    throw new IllegalStateException("Error loading configuration for  " + getClass().getSimpleName() + ". Expected lagom.services." + key + " to be a URI, but it failed to parse", e);
+                }
+            }
+        }
+        this.services = HashTreePMap.from(services);
+    }
 
 
 
-  @Override
-  public CompletionStage<Optional<URI>> locate(String name, Descriptor.Call<?, ?> serviceCall) {
-      return CompletableFuture.completedFuture(Optional.ofNullable(services.get(name)));
-  }
+    @Override
+    public CompletionStage<Optional<URI>> locate(String name, Descriptor.Call<?, ?> serviceCall) {
+        return CompletableFuture.completedFuture(Optional.ofNullable(services.get(name)));
+    }
 
 }
