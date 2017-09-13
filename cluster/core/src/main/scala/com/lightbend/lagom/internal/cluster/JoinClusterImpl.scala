0b2b31aa0fa7e6ff51e852f5fcf4be22d38b0a45
Ignasi Marimon-Clos <ignasi@lightbend.com>
2017-09-13 11:29:11 +0200

    Simplifies the actor system termination and JVM shutdown (#978)
    * Simplifies the actor system termination and JVM shutdown
    
    * Reintroduces exit-jvm-when-system-terminated to control lifecycle in dev and tests
    
    * Improves UX (comments and logs)

--- cluster/core/src/main/scala/com/lightbend/lagom/internal/cluster/JoinClusterImpl.scala
+++ cluster/core/src/main/scala/com/lightbend/lagom/internal/cluster/JoinClusterImpl.scala
@@ -4,13 +4,13 @@
 package com.lightbend.lagom.internal.cluster
 
 import java.util.concurrent.TimeUnit
-import java.util.concurrent.atomic.{ AtomicBoolean, AtomicInteger }
+
+import akka.actor.ActorSystem
+import akka.cluster.Cluster
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.util.Try
-import akka.actor.ActorSystem
-import akka.cluster.Cluster
 
 private[lagom] object JoinClusterImpl {
 
@@ -32,59 +32,33 @@ private[lagom] object JoinClusterImpl {
     if (cluster.settings.SeedNodes.isEmpty && joinSelf)
       cluster.join(cluster.selfAddress)
 
-    val SUCCESSFUL_EXIT = 0
-    val exitStatus = new AtomicInteger(SUCCESSFUL_EXIT)
-
-    val isExiting = new AtomicBoolean(false)
-
-    if (exitJvm) {
-      // exit JVM when ActorSystem has been terminated
-      system.registerOnTermination {
-        new Thread("Sys-exiting-from-akka-termination") {
-          override def run(): Unit = {
-            if (isExiting.compareAndSet(false, true)) {
-              println("Proceed to JVM shutdown with exit status: " + exitStatus.get())
-              System.exit(exitStatus.get())
-            } else {
-              println("JVM shutdown already handled. Ignore.")
-            }
-          }
-        }
-      }
-    }
-
     Cluster(system).registerOnMemberRemoved {
       // The delay is to give ClusterSingleton actors some time to stop gracefully.
       system.scheduler.scheduleOnce(terminateSystemAfter) {
-        val CLUSTER_MEMBERSHIP_REMOVED = -128
-        val EXIT_TIMEOUT_WITH_HALT = -2
-        exitStatus.compareAndSet(SUCCESSFUL_EXIT, CLUSTER_MEMBERSHIP_REMOVED)
 
-        // `needsExiting` must be set before invoking `system.terminate`
-        val needsExiting = isExiting.compareAndSet(false, true)
         system.terminate()
 
         if (exitJvm) {
+
           // In case ActorSystem shutdown takes longer than 10 seconds,
           // exit the JVM forcefully anyway.
           // We must spawn a separate thread to not block current thread,
           // since that would have blocked the shutdown of the ActorSystem.
-          val t = new Thread(new Runnable {
+          new Thread(new Runnable {
             override def run(): Unit = {
               if (Try(Await.ready(system.whenTerminated, 10.seconds)).isFailure) {
                 System.err.println("Halting JVM.")
-                Runtime.getRuntime.halt(EXIT_TIMEOUT_WITH_HALT)
+                Runtime.getRuntime.halt(-2)
               }
+
               // this is reached only when `system.whenTerminated` completes successfully.
-              if (needsExiting) {
-                println("Proceed to JVM shutdown with exit status: " + exitStatus.get())
-                System.exit(exitStatus.get())
-              }
+              val CLUSTER_MEMBERSHIP_REMOVED = -128
+              System.exit(CLUSTER_MEMBERSHIP_REMOVED)
+
             }
-          })
-          t.setDaemon(true)
-          t.start()
+          }).start()
         }
+
       }(system.dispatcher)
 
     }
