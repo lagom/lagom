307c3f69dc3ab07c99d8b63044c1d82491f8b51b
Renato Cavalcanti <renato@lightbend.com>
2017-10-04 17:48:08 +0200

    wrap sql statement call in try / finally block (#1029)
    * wrap sql statement call in try / finally block
    
    * fixes formatting

--- persistence-jdbc/core/src/main/scala/com/lightbend/lagom/internal/persistence/jdbc/SlickProvider.scala
+++ persistence-jdbc/core/src/main/scala/com/lightbend/lagom/internal/persistence/jdbc/SlickProvider.scala
@@ -138,8 +138,11 @@ private[lagom] class SlickProvider(
       DBIO.sequence(schemaStatements.map { s =>
         SimpleDBIO { ctx =>
           val stmt = ctx.connection.createStatement()
-          stmt.executeUpdate(s)
-          stmt.close()
+          try {
+            stmt.executeUpdate(s)
+          } finally {
+            stmt.close()
+          }
         }
       }).asTry.flatMap {
         case Success(_) => DBIO.successful(())
