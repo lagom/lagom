749137983c05369bdbfe00c98ac19e2a6d050391
James Roper <james@jazzy.id.au>
2017-10-12 16:52:17 -0400

    Fixed invalid type signature on PersistentEntityRef.ask
    Fixes #1050.
    
    Type bounds must either be a type variable, or a set of classes
    interfaces, you cannot mix type variables and classes/interfaces in a
    type bounds, but PersistentEntityRef.ask was mixing them.
    
    The effect of this problem was that some Java compilers, in particular,
    IntelliJ's, were rejecting the type signature and so weren't able to
    typecheck the code that used it, resulting in end user code being unable
    to infer the `Reply` type in the `CompletionStage`.
    
    I couldn't come up with any way to fix this and maintain the same level
    of type safety, so I've compromised by dropping the type variable from
    the type bounds, which means we do get an inferred return type from the
    `ReplyType` of the command, but it's not enforced that the command is
    one that the persistent entity accepts. The fact that the class must
    extend `ReplyType` should be, in most cases, enough to prevent most
    errors that users in a typical project would make, especially if they
    only have one persistent entity in their project.
    
    To maintain binary compatibility, I added `Object` as the left most
    typebound of the `Cmd` variable, which will ensure the erased method
    still accepts `Object`, rather than `ReplyType`.
    
    In addition to fixing the invalid signature, I translated the class from
    Scala to Java, to ensure that the new signature is indeed valid, and to
    ensure we don't introduce this regression there.

---
+++ persistence/javadsl/src/main/java/com/lightbend/lagom/javadsl/persistence/PersistentEntityRef.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2016-2017 Lightbend Inc. <https://www.lightbend.com>
+ */
+package com.lightbend.lagom.javadsl.persistence;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.NoSerializationVerificationNeeded;
+import akka.pattern.Patterns;
+import akka.util.Timeout;
+import scala.compat.java8.FutureConverters;
+import scala.concurrent.duration.FiniteDuration;
+
+import java.io.NotSerializableException;
+import java.io.ObjectStreamException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+/**
+ * Commands are sent to a {@link PersistentEntity} using a
+ * <code>PersistentEntityRef</code>. It is retrieved with
+ * {@link PersistentEntityRegistry#refFor(Class, String)}.
+ */
+public final class PersistentEntityRef<Command> implements NoSerializationVerificationNeeded {
+  private final String entityId;
+  private final ActorRef region;
+  private final ActorSystem system;
+  private final Timeout timeout;
+
+  public PersistentEntityRef(String entityId, ActorRef region, ActorSystem system, FiniteDuration askTimeout) {
+    this.entityId = entityId;
+    this.region = region;
+    this.system = system;
+    this.timeout = Timeout.apply(askTimeout);
+  }
+
+  public String entityId() {
+    return entityId;
+  }
+
+  /**
+   * Send the <code>command</code> to the {@link PersistentEntity}. The returned
+   * <code>CompletionStage</code> will be completed with the reply from the <code>PersistentEntity</code>.
+   * The type of the reply is defined by the command (see {@link PersistentEntity.ReplyType}).
+   * <p>
+   * The <code>CompletionStage</code> may also be completed with failure, sent by the <code>PersistentEntity</code>
+   * or a <code>akka.pattern.AskTimeoutException</code> if there is no reply within a timeout.
+   * The timeout can defined in configuration or overridden using {@link #withAskTimeout(FiniteDuration)}.
+   */
+  @SuppressWarnings("unchecked")
+  public <Reply, Cmd extends Object & PersistentEntity.ReplyType<Reply>> CompletionStage<Reply> ask(Cmd command) {
+    CompletionStage<Object> future = FutureConverters.toJava(
+        Patterns.ask(region, new CommandEnvelope(entityId, command), timeout)
+    );
+
+    return future.thenCompose(result -> {
+      if (result instanceof Throwable) {
+        CompletableFuture<Reply> failed = new CompletableFuture<>();
+        failed.completeExceptionally((Throwable) result);
+        return failed;
+      } else {
+        return CompletableFuture.completedFuture((Reply) result);
+      }
+    });
+
+  }
+
+
+  /**
+   * The timeout for {@link #ask(Object)}. The timeout is by default defined in configuration
+   * but it can be adjusted for a specific <code>PersistentEntityRef</code> using this method.
+   * Note that this returns a new <code>PersistentEntityRef</code> instance with the given timeout
+   * (<code>PersistentEntityRef</code> is immutable).
+   */
+  public PersistentEntityRef<Command> withAskTimeout(FiniteDuration timeout) {
+    return new PersistentEntityRef<>(entityId, region, system, timeout);
+  }
+
+  //  Reasons for why we don't not support serialization of the PersistentEntityRef:
+  //  - it will rarely be sent as a message itself, so providing a serializer will not help
+  //  - it will be embedded in other messages and the only way we could support that
+  //    transparently is to implement java serialization (readResolve, writeReplace)
+  //    like ActorRef, but we don't want to encourage java serialization anyway
+  //  - serializing/embedding the entityId String in other messages is simple
+  //  - might be issues with the type `Command`?
+  private Object writeReplace() throws ObjectStreamException {
+    throw new NotSerializableException(getClass().getName() + " is not serializable. Send the entityId instead.");
+  }
+
+  @Override
+  public String toString() {
+    return "PersistentEntityRef(" + entityId + ")";
+  }
+}
