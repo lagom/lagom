1bc11035ee01384c6c68667af27c4ee8a1b46fac
Julien Jean Paul Sirocchi <julien.sirocchi@gmail.com>
2017-10-27 11:46:53 +0100

    PathParamSerializer derived instances for traversables (#1021)
    * add PathParamSerializer derived instances for traversables + minimal
      testing
    
    * using LowPrio implicits
    
    * added PathParaSerializers for Java collection
    (Collection, List, Set)
    
    * added test for Java collections
    
    * fixed test - somehow a line was deleted before commit/pushing

--- service/javadsl/api/src/test/java/com/lightbend/lagom/api/mock/BlogService.java
+++ service/javadsl/api/src/test/java/com/lightbend/lagom/api/mock/BlogService.java
@@ -9,7 +9,10 @@ import com.lightbend.lagom.javadsl.api.Service;
 import com.lightbend.lagom.javadsl.api.ServiceCall;
 import com.lightbend.lagom.javadsl.api.transport.Method;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static com.lightbend.lagom.javadsl.api.Service.*;
 
@@ -47,6 +50,9 @@ public interface BlogService extends Service {
     ServiceCall<NotUsed, NotUsed> getPosts(String blogId, Optional<Integer> pageNo, Optional<Integer> pageSize);
     ServiceCall<NotUsed, NotUsed> getPost(String blogId, long postId);
     ServiceCall<NotUsed, NotUsed> getComment(String blogId, long postId, long commentId);
+    ServiceCall<NotUsed, NotUsed> getCommentRepeatList(String blogId, List<String> repeat);
+    ServiceCall<NotUsed, NotUsed> getCommentRepeatCollection(String blogId, Collection<String> repeat);
+    ServiceCall<NotUsed, NotUsed> getCommentRepeatSet(String blogId, Set<String> repeat);
 
     @Override
     default Descriptor descriptor() {
@@ -54,7 +60,10 @@ public interface BlogService extends Service {
                 restCall(Method.GET, "/blogs/:blogId", this::getBlog),
                 restCall(Method.GET, "/blogs/:blogId/posts?pageNo&pageSize", this::getPosts),
                 restCall(Method.GET, "/blogs/:blogId/posts/:postId", this::getPost),
-                restCall(Method.GET, "/blogs/:blogId/posts/:postId/comments/:commentId", this::getComment)
+                restCall(Method.GET, "/blogs/:blogId/posts/:postId/comments/:commentId", this::getComment),
+                restCall(Method.GET, "/blogs/:blogId/posts/repeatCol?repeat", this::getCommentRepeatCollection),
+                restCall(Method.GET, "/blogs/:blogId/posts/repeatList?repeat", this::getCommentRepeatList),
+                restCall(Method.GET, "/blogs/:blogId/posts/repeatSet?repeat", this::getCommentRepeatSet)
         );
     }
 }
