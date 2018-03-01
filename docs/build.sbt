292032c8ff00049dd2ee73d4ec29144b6f1f56b9
Tim Moore <tim.moore@lightbend.com>
2018-03-01 15:54:39 +1030

    Update Akka to 2.5.11

--- docs/build.sbt
+++ docs/build.sbt
@@ -1,6 +1,6 @@
 val ScalaVersion = "2.12.4"
 
-val AkkaVersion = "2.5.10"
+val AkkaVersion = "2.5.11"
 val JUnitVersion = "4.11"
 val JUnitInterfaceVersion = "0.11"
 val ScalaTestVersion = "3.0.4"
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
   val PlayFileWatchVersion = "1.1.7"
 
   // Also be sure to update AkkaVersion in docs/build.sbt.
-  val AkkaVersion = "2.5.10"
+  val AkkaVersion = "2.5.11"
   val AkkaHttpVersion = "10.0.11"
   // Also be sure to update ScalaVersion in docs/build.sbt.
   val ScalaVersions = Seq("2.12.4", "2.11.12")
