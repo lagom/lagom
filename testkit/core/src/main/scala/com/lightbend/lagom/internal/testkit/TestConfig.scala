5c20b14257482ab41d6f7f2016f9b863df2976e1
jules Ivanic <jules.ivanic@gmail.com>
2018-03-30 16:20:43 +0200

    Do not force the use of H2 in tests
    Fix typos

--- testkit/core/src/main/scala/com/lightbend/lagom/internal/testkit/TestConfig.scala
+++ testkit/core/src/main/scala/com/lightbend/lagom/internal/testkit/TestConfig.scala
@@ -8,11 +8,6 @@ import com.typesafe.config.{ Config, ConfigFactory }
 private[lagom] object TestConfig {
   lazy val JdbcConfig: Config = ConfigFactory.parseString(
     """
-      |db.default.driver=org.h2.Driver
-      |db.default.url="jdbc:h2:mem:service-test"
-      |
-      |jdbc-defaults.slick.profile = "slick.jdbc.H2Profile$"
-      |
       |jdbc-journal.slick.profile = ${?jdbc-defaults.slick.profile}
       |jdbc-read-journal.slick.profile = ${?jdbc-defaults.slick.profile}
       |jdbc-snapshot-store.slick.profile = ${?jdbc-defaults.slick.profile}
