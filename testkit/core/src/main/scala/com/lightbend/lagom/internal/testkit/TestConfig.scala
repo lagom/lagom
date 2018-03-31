a759a1b8adfdd8ea84fda7f6a191144792bda24b
jules Ivanic <jules.ivanic@gmail.com>
2018-03-31 18:31:44 +0200

    Fix tests: Use H2 as fallback configuration

--- testkit/core/src/main/scala/com/lightbend/lagom/internal/testkit/TestConfig.scala
+++ testkit/core/src/main/scala/com/lightbend/lagom/internal/testkit/TestConfig.scala
@@ -5,16 +5,29 @@ package com.lightbend.lagom.internal.testkit
 
 import com.typesafe.config.{ Config, ConfigFactory }
 
+import scala.util.Try
+
 private[lagom] object TestConfig {
-  lazy val JdbcConfig: Config = ConfigFactory.parseString(
-    """
-      |jdbc-journal.slick.profile = ${?jdbc-defaults.slick.profile}
-      |jdbc-read-journal.slick.profile = ${?jdbc-defaults.slick.profile}
-      |jdbc-snapshot-store.slick.profile = ${?jdbc-defaults.slick.profile}
-      |lagom.persistence.read-side.jdbc.slick.profile = ${?jdbc-defaults.slick.profile}
-      |
-      |akka.persistence.journal.plugin = jdbc-journal
-      |akka.persistence.snapshot-store.plugin = jdbc-snapshot-store
+  private lazy val defaults = ConfigFactory.defaultApplication().resolve()
+
+  private def getConfig(key: String, fallbackValue: String): String =
+    s"""$key = "${Try(defaults.getString(key)).getOrElse(fallbackValue)}""""
+
+  private lazy val values =
+    s"""
+       |${getConfig("db.default.driver", "org.h2.Driver")}
+       |${getConfig("db.default.url", "jdbc:h2:mem:service-test")}
+       |
+       |${getConfig("jdbc-defaults.slick.profile", "slick.jdbc.H2Profile$")}
+       |
+       |jdbc-journal.slick.profile = $${?jdbc-defaults.slick.profile}
+       |jdbc-read-journal.slick.profile = $${?jdbc-defaults.slick.profile}
+       |jdbc-snapshot-store.slick.profile = $${?jdbc-defaults.slick.profile}
+       |lagom.persistence.read-side.jdbc.slick.profile = $${?jdbc-defaults.slick.profile}
+       |
+       |akka.persistence.journal.plugin = jdbc-journal
+       |akka.persistence.snapshot-store.plugin = jdbc-snapshot-store
     """.stripMargin
-  ).resolve()
+
+  lazy val JdbcConfig: Config = ConfigFactory.parseString(values).resolve()
 }
