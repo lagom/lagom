4e8be17ef6b8c4e6ad41a69d9d66bc7009be68d5
Ignasi Marimon-Clos <ignasi@lightbend.com>
2018-04-03 20:05:27 +0200

    Sets a User-Agent so cloudflare won't block requests

--- project/Doc.scala
+++ project/Doc.scala
@@ -88,12 +88,6 @@ object UnidocRoot extends AutoPlugin {
     case _ => Seq("--allow-script-in-comments")
   }
 
-
-  private def packageListLocation(baseDir:File, name:String) = {
-    baseDir / "docs"/"src"/"main"/"resources"/"package-list"/ name
-  }
-
-
   override lazy val projectSettings = scalaJavaUnidocSettings ++ Seq(
     unidocAllSources in (JavaUnidoc, unidoc) ++= allGenjavadocSources.value,
     unidocAllSources in (JavaUnidoc, unidoc) := {
@@ -105,10 +99,11 @@ object UnidocRoot extends AutoPlugin {
     scalacOptions in (ScalaUnidoc, unidoc) ++= Seq("-skip-packages", "com.lightbend.lagom.internal"),
     javacOptions in doc := Seq(
       "-windowtitle", "Lagom Services API",
+      "-J-Dhttp.agent=Lagom-Unidoc-Javadoc",
       "-link", "https://docs.oracle.com/javase/8/docs/api/",
-      "-linkoffline", "https://doc.akka.io/japi/akka/current/", packageListLocation(baseDirectory.value, "akka").getAbsolutePath,
-      "-linkoffline", "https://doc.akka.io/japi/akka-http/current/", packageListLocation(baseDirectory.value, "akka-http").getAbsolutePath,
-      "-linkoffline", "https://www.playframework.com/documentation/latest/api/java/", packageListLocation(baseDirectory.value, "play").getAbsolutePath,
+      "-link", "https://doc.akka.io/japi/akka/current/",
+      "-link", "https://doc.akka.io/japi/akka-http/current/",
+      "-link", "https://www.playframework.com/documentation/latest/api/java/",
       "-public",
       "-group", "Services API", packageList(
         "com.lightbend.lagom.javadsl",
