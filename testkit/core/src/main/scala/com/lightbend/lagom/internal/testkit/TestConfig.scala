bdb759c80e91f343b6ea985fe9d164375eeadea3
jules Ivanic <jules.ivanic@gmail.com>
2018-04-03 14:21:26 +0200

    @ignasi35 review: Prefer the use of a `reference.conf` file for JDBC tests configuration

--- testkit/core/src/main/scala/com/lightbend/lagom/internal/testkit/TestConfig.scala
+++ testkit/core/src/main/scala/com/lightbend/lagom/internal/testkit/TestConfig.scala
@@ -5,26 +5,9 @@ package com.lightbend.lagom.internal.testkit
 
 import com.typesafe.config.{ Config, ConfigFactory }
 
-import scala.util.Try
-
 private[lagom] object TestConfig {
-  private lazy val userConfigs = ConfigFactory.load()
-
-  private def getConfig(key: String, fallbackValue: String): String =
-    s"""$key = "${Try(userConfigs.getString(key)).getOrElse(fallbackValue)}""""
-
   private lazy val values =
     s"""
-       |${getConfig("db.default.driver", "org.h2.Driver")}
-       |${getConfig("db.default.url", "jdbc:h2:mem:service-test")}
-       |
-       |${getConfig("jdbc-defaults.slick.profile", "slick.jdbc.H2Profile$")}
-       |
-       |jdbc-journal.slick.profile = $${?jdbc-defaults.slick.profile}
-       |jdbc-read-journal.slick.profile = $${?jdbc-defaults.slick.profile}
-       |jdbc-snapshot-store.slick.profile = $${?jdbc-defaults.slick.profile}
-       |lagom.persistence.read-side.jdbc.slick.profile = $${?jdbc-defaults.slick.profile}
-       |
        |akka.persistence.journal.plugin = jdbc-journal
        |akka.persistence.snapshot-store.plugin = jdbc-snapshot-store
     """.stripMargin
