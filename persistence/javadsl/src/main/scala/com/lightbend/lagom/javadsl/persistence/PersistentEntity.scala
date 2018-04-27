93e8037606abc19554150626001708f586aee6f3
Tim Moore <tim.moore@lightbend.com>
2018-04-27 13:06:52 +0930

    Fix inline link syntax

--- persistence/javadsl/src/main/scala/com/lightbend/lagom/javadsl/persistence/PersistentEntity.scala
+++ persistence/javadsl/src/main/scala/com/lightbend/lagom/javadsl/persistence/PersistentEntity.scala
@@ -259,7 +259,7 @@ abstract class PersistentEntity[Command, Event, State] {
 
   /**
    * Mutable builder that is used for defining the event and command handlers.
-   * Use [#build] to create the immutable [[PersistentEntity.Behavior]].
+   * Use [[BehaviorBuilder#build]] to create the immutable [[PersistentEntity.Behavior]].
    */
   // In order to provide javadoc preventing instantiation or extension this sealed trait is added
   // to hold docs and BehaviorBuilderImpl is made private to hold implementation
