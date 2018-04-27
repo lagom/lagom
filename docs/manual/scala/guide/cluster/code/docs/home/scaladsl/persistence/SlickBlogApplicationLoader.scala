0f99029f8daeb844a38ff4f32790134d61a38577
Renato  Cavalcanti <renato@cavalcanti.be>
2018-04-27 16:53:25 +0200

    fix code fragment compilation fro docus

--- docs/manual/scala/guide/cluster/code/docs/home/scaladsl/persistence/SlickBlogApplicationLoader.scala
+++ docs/manual/scala/guide/cluster/code/docs/home/scaladsl/persistence/SlickBlogApplicationLoader.scala
@@ -1,9 +1,18 @@
 /*
  * Copyright (C) 2016-2018 Lightbend Inc. <https://www.lightbend.com>
  */
+package docs.home.scaladsl.persistence
+
+import com.lightbend.lagom.scaladsl.server.{LagomApplication, LagomApplicationContext, LagomServer}
+import play.api.libs.ws.ahc.AhcWSComponents
+import play.api.db.HikariCPComponents
+import com.softwaremill.macwire._
+import com.lightbend.lagom.scaladsl.persistence.jdbc._
+import com.lightbend.lagom.scaladsl.persistence.slick._
+
 
 //#load-components
-abstract class BlogApplication(context: LagomApplicationContext)
+abstract class SlickBlogApplication(context: LagomApplicationContext)
   extends LagomApplication(context)
     with JdbcPersistenceComponents
     with SlickPersistenceComponents
@@ -13,6 +22,8 @@ abstract class BlogApplication(context: LagomApplicationContext)
 
   // Bind the services that this server provides
   override lazy val lagomServer = ???
+
+  lazy val myDatabase: MyDatabase = MyDatabase
 
   // Register the JSON serializer registry
   override lazy val jsonSerializerRegistry = BlogPostSerializerRegistry
