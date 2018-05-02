aa7a136496ea062ed818db7deb4fbb7e991d7618
Kyle Florence <kyle.florence@gmail.com>
2018-05-02 14:03:49 -0700

    Add mention of SlickPersistentComponents. #1105 (#1323)
    * Add mention of SlickPersistentComponents. #1105
    
    * fix code fragment compilation fro docus
    
    * Small semantic update to copy

---
+++ docs/manual/scala/guide/cluster/code/docs/home/scaladsl/persistence/SlickBlogApplicationLoader.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2016-2018 Lightbend Inc. <https://www.lightbend.com>
+ */
+package docs.home.scaladsl.persistence
+
+import com.lightbend.lagom.scaladsl.server.{LagomApplication, LagomApplicationContext, LagomServer}
+import play.api.libs.ws.ahc.AhcWSComponents
+import play.api.db.HikariCPComponents
+import com.softwaremill.macwire._
+import com.lightbend.lagom.scaladsl.persistence.jdbc._
+import com.lightbend.lagom.scaladsl.persistence.slick._
+
+
+//#load-components
+abstract class SlickBlogApplication(context: LagomApplicationContext)
+  extends LagomApplication(context)
+    with JdbcPersistenceComponents
+    with SlickPersistenceComponents
+    with HikariCPComponents
+    with AhcWSComponents {
+//#load-components
+
+  // Bind the services that this server provides
+  override lazy val lagomServer = ???
+
+  lazy val myDatabase: MyDatabase = MyDatabase
+
+  // Register the JSON serializer registry
+  override lazy val jsonSerializerRegistry = BlogPostSerializerRegistry
+
+  // Register the Blog application persistent entity
+  persistentEntityRegistry.register(wire[Post])
+
+  // Register the event processor
+  //#register-event-processor
+  readSide.register(wire[BlogEventProcessor])
+  //#register-event-processor
+}
