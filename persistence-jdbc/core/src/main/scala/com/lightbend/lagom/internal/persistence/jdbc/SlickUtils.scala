5e57267cba9aaa6bb6d798aa9fe59cf40d074229
Renato  Cavalcanti <renato@cavalcanti.be>
2018-05-29 09:07:14 +0200

    copyright header

--- persistence-jdbc/core/src/main/scala/com/lightbend/lagom/internal/persistence/jdbc/SlickUtils.scala
+++ persistence-jdbc/core/src/main/scala/com/lightbend/lagom/internal/persistence/jdbc/SlickUtils.scala
@@ -1,13 +1,33 @@
+/*
+ * Copyright 2016 Dennis Vriend
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.lightbend.lagom.internal.persistence.jdbc
 
 import javax.naming.InitialContext
-
 import akka.persistence.jdbc.config.SlickConfiguration
 import com.typesafe.config.Config
 import slick.basic.DatabaseConfig
 import slick.jdbc.JdbcProfile
 import slick.jdbc.JdbcBackend._
 
+/*
+ * Code extracted from
+ * https://github.com/dnvriend/akka-persistence-jdbc/blob/master/src/main/scala/akka/persistence/jdbc/util/SlickDriver.scala#L27
+ *  and
+ * https://github.com/dnvriend/akka-persistence-jdbc/blob/master/src/main/scala/akka/persistence/jdbc/util/SlickDriver.scala#L32
+ */
 object SlickUtils {
 
   def forDriverName(config: Config): JdbcProfile =
