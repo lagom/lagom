39c156e03b7877d090ece950437b8202f2d749f7
James Roper <james@jazzy.id.au>
2018-05-30 23:29:09 +1000

    Improved error reporting for serializer resolution (#1362)
    * Improved error reporting for serializer resolution
    
    In this case, ensured that if type variables are used in the message
    type, the serializer resolution fails with a meaningful error message.
    
    * Review error message.

---
+++ service/javadsl/api/src/test/java/com/lightbend/lagom/api/mock/InvalidMessageTypeService.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2016-2018 Lightbend Inc. <https://www.lightbend.com>
+ */
+package com.lightbend.lagom.api.mock;
+
+import com.lightbend.lagom.javadsl.api.Descriptor;
+import com.lightbend.lagom.javadsl.api.Service;
+import com.lightbend.lagom.javadsl.api.ServiceCall;
+import com.lightbend.lagom.javadsl.api.transport.Method;
+
+import static com.lightbend.lagom.javadsl.api.Service.*;
+
+public interface InvalidMessageTypeService extends Service {
+    <Foo> ServiceCall<Foo, String> hello();
+
+    @Override
+    default Descriptor descriptor() {
+        return named("/invalid-service")
+            .withCalls(restCall(Method.POST, "/hello", this::hello));
+    }
+}
