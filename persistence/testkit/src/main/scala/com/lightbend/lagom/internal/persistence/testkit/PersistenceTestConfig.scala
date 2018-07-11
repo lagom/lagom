6afa358a9d6349f88bb645f805c451c242689466
Tim Moore <tim.moore@lightbend.com>
2018-07-11 18:51:27 +0930

    Reorder config keys

--- persistence/testkit/src/main/scala/com/lightbend/lagom/internal/persistence/testkit/PersistenceTestConfig.scala
+++ persistence/testkit/src/main/scala/com/lightbend/lagom/internal/persistence/testkit/PersistenceTestConfig.scala
@@ -8,32 +8,38 @@ import com.typesafe.config.{ Config, ConfigFactory }
 
 private[lagom] object PersistenceTestConfig {
   lazy val ClusterConfigMap: Map[String, AnyRef] = Map(
-    "lagom.cluster.join-self" -> "on",
     "akka.actor.provider" -> "akka.cluster.ClusterActorRefProvider",
+
+    "akka.remote.netty.tcp.hostname" -> "127.0.0.1",
     "akka.remote.netty.tcp.port" -> "0",
-    "akka.remote.netty.tcp.hostname" -> "127.0.0.1"
+
+    "lagom.cluster.join-self" -> "on"
   )
 
   lazy val ClusterConfig: Config = ConfigFactory.parseMap(ClusterConfigMap.asJava)
 
   def cassandraConfigMap(keyspacePrefix: String, cassandraPort: Int): Map[String, AnyRef] = Map(
-    "cassandra-journal.session-provider" -> "akka.persistence.cassandra.ConfigSessionProvider",
-    "cassandra-snapshot-store.session-provider" -> "akka.persistence.cassandra.ConfigSessionProvider",
-    "lagom.persistence.read-side.cassandra.session-provider" -> "akka.persistence.cassandra.ConfigSessionProvider",
     "akka.persistence.journal.plugin" -> "cassandra-journal",
     "akka.persistence.snapshot-store.plugin" -> "cassandra-snapshot-store",
-    "cassandra-journal.port" -> cassandraPort.toString,
-    "cassandra-journal.keyspace" -> keyspacePrefix,
+
+    "akka.test.single-expect-default" -> "5s",
+
     "cassandra-journal.contact-points.0" -> "127.0.0.1",
-    "cassandra-snapshot-store.port" -> cassandraPort.toString,
-    "cassandra-snapshot-store.keyspace" -> keyspacePrefix,
-    "cassandra-snapshot-store.contact-points.0" -> "127.0.0.1",
-    "lagom.persistence.read-side.cassandra.port" -> cassandraPort.toString,
-    "lagom.persistence.read-side.cassandra.keyspace" -> s"${keyspacePrefix}_read",
-    "lagom.persistence.read-side.cassandra.contact-points.0" -> "127.0.0.1",
+    "cassandra-journal.keyspace" -> keyspacePrefix,
+    "cassandra-journal.port" -> cassandraPort.toString,
+    "cassandra-journal.session-provider" -> "akka.persistence.cassandra.ConfigSessionProvider",
 
     "cassandra-query-journal.eventual-consistency-delay" -> "2s",
-    "akka.test.single-expect-default" -> "5s"
+
+    "cassandra-snapshot-store.contact-points.0" -> "127.0.0.1",
+    "cassandra-snapshot-store.keyspace" -> keyspacePrefix,
+    "cassandra-snapshot-store.port" -> cassandraPort.toString,
+    "cassandra-snapshot-store.session-provider" -> "akka.persistence.cassandra.ConfigSessionProvider",
+
+    "lagom.persistence.read-side.cassandra.contact-points.0" -> "127.0.0.1",
+    "lagom.persistence.read-side.cassandra.keyspace" -> s"${keyspacePrefix}_read",
+    "lagom.persistence.read-side.cassandra.port" -> cassandraPort.toString,
+    "lagom.persistence.read-side.cassandra.session-provider" -> "akka.persistence.cassandra.ConfigSessionProvider"
   )
 
   def cassandraConfig(keyspacePrefix: String, cassandraPort: Int): Config =
