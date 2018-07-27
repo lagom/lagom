c7c19652a7954c140bfcbcd57153266c5052b576
Ignasi Marimon-Clos <ignasi@lightbend.com>
2018-07-27 18:55:11 +0200

    Adds validation over actual values

--- project/LagomPublish.scala
+++ project/LagomPublish.scala
@@ -1,27 +1,32 @@
-import Dependencies.{ validateDependencies, validateDependenciesTask }
+import sbt.Keys.{ name, publishTo }
 import sbt.{ Def, FeedbackProvidedException, Resolver, Task, taskKey }
-import sbt.Keys.{ name, publishTo, streams }
 
 object LagomPublish {
 
   val validatePublishSettings = taskKey[Unit]("Validate Lagom settings to publish released artifacts.")
 
   val validatePublishSettingsTask: Def.Initialize[Task[Unit]] = Def.task {
-    val ptValue: Option[Resolver] = publishTo.value
+    val resolverValue: Option[Resolver] = publishTo.value
 
-    val log = streams.value.log
-
-    if (ptValue.isEmpty) {
-      throw PublishValidationFailed
-    }else{
-       log.info(s"[${name.value}] PublishTo settings validation passed.")
+    // the following implements the rules described in https://github.com/lagom/lagom/issues/1496#issuecomment-408398508
+    // TODO: improve rules and validations depending on the version (SNAPSHOT vs release)
+    (name.value, resolverValue) match {
+      case (_, None) => throw new PublishValidationFailed("`publishTo` not set.")
+      case ("lagom-sbt-plugin", x) =>
+        if (x.get.name != "Bintray-Sbt-Publish-lagom-sbt-plugin-releases-lagom-sbt-plugin") {
+          throw new PublishValidationFailed("""Invalid resolver. Expected: "Raw(Bintray-Sbt-Publish-lagom-sbt-plugin-releases-lagom-sbt-plugin)".""")
+        }
+      case (_, x) =>
+        if (!x.get.name.toLowerCase.contains("sonatype")) {
+          throw new PublishValidationFailed(s"""Invalid resolver. Expected: Sonatype. Actual $x.""")
+        }
     }
   }
 
   val validatePublishSettingsSetting = validatePublishSettings := validatePublishSettingsTask.value
 
-  private object PublishValidationFailed extends RuntimeException with FeedbackProvidedException {
-    override def toString = "PublishTo settings validation failed!"
+  private class PublishValidationFailed(message: String) extends RuntimeException with FeedbackProvidedException {
+    override def toString = message
   }
 
 }
