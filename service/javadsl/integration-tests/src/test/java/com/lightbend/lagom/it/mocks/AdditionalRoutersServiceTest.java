2755f7b37cb8f2b242af54409c2ec564fa3a9e0b
Renato  Cavalcanti <renato@cavalcanti.be>
2018-08-02 11:56:13 +0200

    improved tests

--- service/javadsl/integration-tests/src/test/java/com/lightbend/lagom/it/mocks/AdditionalRoutersServiceTest.java
+++ service/javadsl/integration-tests/src/test/java/com/lightbend/lagom/it/mocks/AdditionalRoutersServiceTest.java
@@ -1,8 +1,11 @@
 /*
  * Copyright (C) 2016-2018 Lightbend Inc. <https://www.lightbend.com>
  */
+
 package com.lightbend.lagom.it.mocks;
 
+import akka.actor.ActorSystem;
+import akka.stream.ActorMaterializer;
 import com.lightbend.lagom.javadsl.testkit.ServiceTest;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -15,15 +18,22 @@ import static com.lightbend.lagom.javadsl.testkit.ServiceTest.startServer;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+
 import play.test.*;
+
 import static play.test.Helpers.*;
 
 public class AdditionalRoutersServiceTest {
 
     private static ServiceTest.TestServer server;
 
+    private static ActorSystem system;
+    private static ActorMaterializer materializer;
+
     @BeforeClass
     public static void setUp() {
+        system = ActorSystem.create();
+        materializer = ActorMaterializer.create(system);
         server = startServer(defaultSetup()
             .withCluster(false).withCassandra(false)
             .configureBuilder(b -> b.bindings(new AdditionalRoutersServiceModule()))
@@ -36,6 +46,14 @@ public class AdditionalRoutersServiceTest {
             server.stop();
             server = null;
         }
+        if (materializer != null) {
+            materializer.shutdown();
+            materializer = null;
+        }
+        if (system != null) {
+            system.terminate();
+            system = null;
+        }
     }
 
     @Test
@@ -46,6 +64,7 @@ public class AdditionalRoutersServiceTest {
 
         Result pingRes = route(server.app(), reqPing);
         assertEquals(OK, pingRes.status());
+        assertEquals(pingRes.body().consumeData(materializer).toCompletableFuture().get().utf8String(), "ping");
 
         Http.RequestBuilder reqPong = Helpers.fakeRequest()
             .method(GET)
@@ -53,8 +72,9 @@ public class AdditionalRoutersServiceTest {
 
         Result pongRes = route(server.app(), reqPong);
         assertEquals(OK, pongRes.status());
+         // FIXME this actually gets a PING! :O
+        assertEquals(pongRes.body().consumeData(materializer).toCompletableFuture().get().utf8String(), "pong");
     }
-
 
 
 }
