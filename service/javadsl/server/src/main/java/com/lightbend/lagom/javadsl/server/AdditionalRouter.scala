2dfbd0de09ea413c7e636128c9c41b050337a03e
Renato  Cavalcanti <renato@cavalcanti.be>
2018-08-03 18:10:11 +0200

    better simulate Play's withPrefix behavior

--- service/javadsl/server/src/main/java/com/lightbend/lagom/javadsl/server/AdditionalRouter.scala
+++ service/javadsl/server/src/main/java/com/lightbend/lagom/javadsl/server/AdditionalRouter.scala
@@ -21,18 +21,26 @@ final case class ClassBased[R <: Router](classType: Class[R], prefix: Option[Str
 
   def this(classType: Class[R]) = this(classType, None)
 
-  def withPrefix(path: String): ClassBased[R] = copy(prefix = Option(path))
+  def withPrefix(path: String): ClassBased[R] = copy(prefix = AdditionalRouter.appendPrefix(prefix, path))
 }
 
 final case class InstanceBased(router: Router, prefix: Option[String]) extends AdditionalRouter {
 
   def this(classType: Router) = this(classType, None)
 
-  def withPrefix(path: String): InstanceBased = copy(prefix = Option(path))
+  def withPrefix(path: String): InstanceBased = copy(prefix = AdditionalRouter.appendPrefix(prefix, path))
 }
 
 object AdditionalRouter {
 
+  /**
+   * Simulate Play's withPrefix behavior by prepending new prefix to an existing path.
+   */
+  private[lagom] def appendPrefix(prefix: Option[String], newPrefix: String) = {
+    prefix
+      .map(oldPrefix => newPrefix + "/" + oldPrefix)
+      .orElse(Option(newPrefix))
+  }
   private[lagom] def wireRouters(injector: Injector, additionalRouters: util.List[AdditionalRouter]): util.List[Router] = {
 
     // modifies the Router in case a prefix is defined
