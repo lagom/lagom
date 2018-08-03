526256f987a5f187c67f531d2c683aea518874b1
Ignasi Marimon-Clos <ignasi@lightbend.com>
2018-08-03 14:18:12 +0200

    Simplifies code

--- project/LagomPublish.scala
+++ project/LagomPublish.scala
@@ -17,14 +17,14 @@ object LagomPublish {
         // see https://github.com/sbt/sbt-bintray/blob/7c93bacaae3ffc128564ceacb6e73ec4486525dd/src/main/scala/Bintray.scala#L16-L29 for
         // details on the syntax of Bintray Resolver names.
         if (inReleaseVersion && x.get.name != "Bintray-Sbt-Publish-lagom-sbt-plugin-releases-lagom-sbt-plugin") {
-          throw new PublishValidationFailed(s"""Invalid resolver. Expected: "Raw(Bintray-Sbt-Publish-lagom-sbt-plugin-releases-lagom-sbt-plugin)" but was "${x.get.name}".""")
+          throw new PublishValidationFailed("Raw(Bintray-Sbt-Publish-lagom-sbt-plugin-releases-lagom-sbt-plugin)", x.get)
         }
         // TODO: Add a validation for "lagom-sbt-plugin" when the version is a snapshot.
       case (_, x) =>
         // TODO: this could be improved to assert the specific Resolver depending on release-vs-snapshot nature of the version.
         // e.g. sonatype-staging vs sonatype-snapshots
         if (!x.get.name.toLowerCase.contains("sonatype")) {
-          throw new PublishValidationFailed(s"""Invalid resolver. Expected: Sonatype. Actual ${x.get}.""")
+          throw new PublishValidationFailed("Sonatype", x.get)
         }
     }
   }
@@ -32,7 +32,8 @@ object LagomPublish {
   val validatePublishSettingsSetting = validatePublishSettings := validatePublishSettingsTask.value
 
 
-  private class PublishValidationFailed(message: String) extends RuntimeException with FeedbackProvidedException {
+  private class PublishValidationFailed(message:String) extends RuntimeException with FeedbackProvidedException {
+    def this(expectedResolver: String, actual:Resolver) = this(s"""Invalid resolver. Expected: "$expectedResolver" but was "$actual".""")
     override def toString = message
   }
 
