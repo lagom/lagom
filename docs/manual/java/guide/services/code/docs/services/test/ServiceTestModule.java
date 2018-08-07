43b1ace2ad99e4e275f221a13b39a57ea58d09d9
Renato  Cavalcanti <renato@cavalcanti.be>
2018-08-07 18:16:59 +0200

    fix tests in docs

--- docs/manual/java/guide/services/code/docs/services/test/ServiceTestModule.java
+++ docs/manual/java/guide/services/code/docs/services/test/ServiceTestModule.java
@@ -1,6 +1,7 @@
 package docs.services.test;
 
 import com.google.inject.Binder;
+import com.google.inject.TypeLiteral;
 import com.google.inject.AbstractModule;
 import com.lightbend.lagom.internal.javadsl.BinderAccessor;
 import com.lightbend.lagom.internal.javadsl.server.JavadslServicesRouter;
@@ -14,8 +15,10 @@ import com.lightbend.lagom.javadsl.server.ServiceGuiceSupport;
 import com.lightbend.lagom.javadsl.server.status.MetricsService;
 import docs.services.HelloService;
 import docs.services.HelloServiceImpl;
-
+import java.util.Collections;
+import java.util.List;
 import java.util.Arrays;
+import play.api.routing.Router;
 
 public class ServiceTestModule extends AbstractModule implements ServiceGuiceSupport {
 
@@ -75,6 +78,7 @@ public class ServiceTestModule extends AbstractModule implements ServiceGuiceSup
     // Bind the resolved services
     binder.bind(ResolvedServices.class).toProvider(new ResolvedServicesProvider(allServiceBindings));
 
+    binder.bind(new TypeLiteral<List<Router>>(){}).toInstance(Collections.emptyList());
     // And bind the router
     binder.bind(LagomServiceRouter.class).to(JavadslServicesRouter.class);
   }
