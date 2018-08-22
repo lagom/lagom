ed6ab7c91ff9b0b3ea7b93c379b1941ce00c4488
Ayush Prashar <ayush.prashar@gmail.com>
2018-08-22 20:53:14 +0530

    Deprecated state getter setters in behavior builder and its IMPL (#1541)
    * Deprecated state getter setters in behavior builder and its IMPL
    
    * Depracations were provided messages
    
    * Scaliform formatted
    
    * Changed the deprecated message

--- persistence/javadsl/src/main/scala/com/lightbend/lagom/javadsl/persistence/PersistentEntity.scala
+++ persistence/javadsl/src/main/scala/com/lightbend/lagom/javadsl/persistence/PersistentEntity.scala
@@ -211,8 +211,10 @@ abstract class PersistentEntity[Command, Event, State] {
     private var commandHandlers: Map[Class[_ <: Command], JBiFunction[_ <: Command, CommandContext[Any], Persist[_ <: Event]]] =
       cmdHandlers
 
+    @scala.deprecated("The method is deprecated because it was deemed obsolete", since = "1.5.0")
     def getState(): State = _state
 
+    @scala.deprecated("The method is deprecated because it was deemed obsolete", since = "1.5.0")
     def setState(state: State): Unit = {
       _state = state
     }
@@ -268,7 +270,9 @@ abstract class PersistentEntity[Command, Event, State] {
   // This would be legal in Scala, but causes inter-operation problems with some Java compilers (such as Eclipse).
   // See https://github.com/lagom/lagom/pull/1395
   sealed abstract class BehaviorBuilder {
+    @scala.deprecated("The method is deprecated because it was deemed obsolete", since = "1.5.0")
     def getState(): State
+    @scala.deprecated("The method is deprecated because it was deemed obsolete", since = "1.5.0")
     def setState(state: State): Unit
     /**
      * Register an event handler for a given event class. The `handler` function
