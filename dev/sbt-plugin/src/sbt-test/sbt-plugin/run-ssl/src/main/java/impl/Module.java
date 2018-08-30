d70d2379e7a55fdf8bc1e36f53b639eddfd9f20d
Ignasi Marimon-Clos <ignasi@lightbend.com>
2018-08-30 19:11:46 +0200

    Adds a scripted tests asserting HTTPS availability in dev mode

---
+++ dev/sbt-plugin/src/sbt-test/sbt-plugin/run-ssl/src/main/java/impl/Module.java
@@ -0,0 +1,18 @@
+package impl;
+
+import com.google.inject.AbstractModule;
+import com.lightbend.lagom.javadsl.server.ServiceGuiceSupport;
+import api.FooService;
+import play.*;
+import javax.inject.Inject;
+import java.util.Date;
+import java.io.*;
+
+import com.typesafe.config.Config;
+
+public class Module extends AbstractModule implements ServiceGuiceSupport {
+	@Override
+	protected void configure() {
+		bindService(FooService.class, FooServiceImpl.class);
+	}
+}
