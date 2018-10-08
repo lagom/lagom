3a843ca26b25b88d0e98883ef6aacbc23109e86e
Dale Wijnand <344610+dwijnand@users.noreply.github.com>
2018-10-08 13:54:55 +0100

    Upgrade ssl-config, remove LagomIO, & reconcile SSLContextProvider from Lagom's Dev Mode (#1610)
    Uses the FakeKeyStore in ssl-config, rather than the one in Play.
    Remove LagomIO & SSLContextProvider from Lagom's code base.
    
    ## References
    
    Fixes https://github.com/playframework/playframework/issues/8587
    Fixes #1612

--- testkit/core/src/main/scala/com/lightbend/lagom/internal/testkit/TestkitSslSetup.scala
+++ testkit/core/src/main/scala/com/lightbend/lagom/internal/testkit/TestkitSslSetup.scala
@@ -12,33 +12,44 @@ private[lagom] object TestkitSslSetup {
 
   sealed trait TestkitSslSetup {
     def sslPort: Option[Int]
+
     def sslSettings: Map[String, AnyRef]
-    def sslContext: Option[SSLContext]
+
+    def clientSslContext: Option[SSLContext]
   }
+
   case object Disabled extends TestkitSslSetup {
     val sslPort: Option[Int] = None
     val sslSettings: Map[String, AnyRef] = Map.empty[String, AnyRef]
-    val sslContext: Option[SSLContext] = None
+    val clientSslContext: Option[SSLContext] = None
   }
+
   case class Enabled(
-    sslPort:     Option[Int]         = Some(0),
-    sslSettings: Map[String, AnyRef],
-    sslContext:  Option[SSLContext]
+    sslPort:          Option[Int]         = Some(0),
+    sslSettings:      Map[String, AnyRef],
+    clientSslContext: Option[SSLContext]
   ) extends TestkitSslSetup
 
   val disabled: TestkitSslSetup = Disabled
 
-  def enabled(keystoreFilePath: File, sslContext: SSLContext): TestkitSslSetup = {
+  /**
+   *
+   * @param serverKeyStoreFile keyStore to setup the server
+   * @param trustStoreFile     trustStore for the clients
+   * @param clientSslContext   SSLContext to create SSL clients
+   * @return
+   */
+  def enabled(serverKeyStoreFile: File, trustStoreFile: File, clientSslContext: SSLContext): TestkitSslSetup = {
     val sslSettings: Map[String, AnyRef] = Map(
       // See also play/core/server/LagomReloadableDevServerStart.scala
       // These configure the server
-      "play.server.https.keyStore.path" -> keystoreFilePath.getAbsolutePath,
+      "play.server.https.keyStore.path" -> serverKeyStoreFile.getAbsolutePath,
       "play.server.https.keyStore.type" -> "JKS",
       // These configure the clients (play-ws and akka-grpc)
       "ssl-config.loose.disableHostnameVerification" -> "true",
       "ssl-config.trustManager.stores.0.type" -> "JKS",
-      "ssl-config.trustManager.stores.0.path" -> keystoreFilePath.getAbsolutePath
+      "ssl-config.trustManager.stores.0.path" -> trustStoreFile.getAbsolutePath
     )
-    Enabled(Some(0), sslSettings, Some(sslContext))
+    Enabled(Some(0), sslSettings, Some(clientSslContext))
   }
 }
