96f6669dce30b80c4e1f5a74845aa22a15f763b3
Marcos Pereira <marcos.silva@gmail.com>
2018-12-03 15:38:50 -0500

    Update other dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,21 +26,28 @@ object Dependencies {
     val SbtScala = Seq("2.10.7", "2.12.7")
     val AkkaPersistenceCassandra = "0.61"
     val AkkaPersistenceJdbc = "3.4.0"
+
+    val Disruptor = "3.3.11"
+
     // Also be sure to update ScalaTestVersion in docs/build.sbt.
-    val ScalaTest = "3.0.4"
+    val ScalaTest = "3.0.5"
     val Jackson = "2.9.7"
     val JacksonCore = Jackson
     val JacksonDatatype = Jackson
     val JacksonDatabind = Jackson
     val Guava = "27.0-jre"
     val Maven = "3.3.9"
-    val Netty = "4.1.31.Final"
+    val Netty = "4.1.32.Final"
     val NettyReactiveStreams = "2.0.0"
-    val Kafka = "2.0.0"
+    val Kafka = "2.0.1"
     val AlpakkaKafka = "1.0-M1"
+    val Curator = "2.12.0"
+    val Immutables = "2.3.10"
+    val HibernateCore = "5.3.7.Final"
+    val PCollections = "2.2.0"
 
     val ScalaJava8Compat = "0.9.0"
-    val ScalaXml = "1.1.0"
+    val ScalaXml = "1.1.1"
     val Slick = "3.2.3"
     // Also be sure to update JUnitVersion in docs/build.sbt.
     val JUnit = "4.12"
@@ -50,7 +57,7 @@ object Dependencies {
     val Slf4j = "1.7.25"
     val Logback = "1.2.3"
     // Also be sure to update Log4jVersion in docs/build.sbt.
-    val Log4j = "2.10.0"
+    val Log4j = "2.11.1"
 
     val jetty = "9.4.12.v20180830"
 
@@ -76,11 +83,11 @@ object Dependencies {
   private val guava = "com.google.guava" % "guava" % Versions.Guava
   private val scalaJava8Compat = "org.scala-lang.modules" %% "scala-java8-compat" % Versions.ScalaJava8Compat
   private val scalaXml = "org.scala-lang.modules" %% "scala-xml" % Versions.ScalaXml
-  private val javassist = "org.javassist" % "javassist" % "3.21.0-GA"
+  private val javassist = "org.javassist" % "javassist" % "3.24.0-GA"
   private val scalaParserCombinators = "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.1"
   private val typesafeConfig = "com.typesafe" % "config" % "1.3.3"
   private val sslConfig = "com.typesafe" %% "ssl-config-core" % "0.3.7"
-  private val h2 = "com.h2database" % "h2" % "1.4.192"
+  private val h2 = "com.h2database" % "h2" % "1.4.197"
   private val cassandraDriverCore = "com.datastax.cassandra" % "cassandra-driver-core" % "3.6.0" excludeAll (excludeSlf4j: _*)
 
   private val akkaActor = "com.typesafe.akka" %% "akka-actor" % Versions.Akka
@@ -128,10 +135,12 @@ object Dependencies {
   private val playFunctional = "com.typesafe.play" %% "play-functional" % Versions.PlayJson excludeAll (excludeSlf4j: _*)
   private val playFileWatch = "com.lightbend.play" %% "play-file-watch" % Versions.PlayFileWatch excludeAll (excludeSlf4j: _*)
 
-  private val junit = "junit" % "junit" % Versions.JUnit
-  private val commonsLang = "org.apache.commons" % "commons-lang3" % "3.8"
+  private val pcollections = "org.pcollections" % "pcollections" % Versions.PCollections
 
-  private val dropwizardMetricsCore = "io.dropwizard.metrics" % "metrics-core" % "3.2.2" excludeAll (excludeSlf4j: _*)
+  private val junit = "junit" % "junit" % Versions.JUnit
+  private val commonsLang = "org.apache.commons" % "commons-lang3" % "3.8.1"
+
+  private val dropwizardMetricsCore = "io.dropwizard.metrics" % "metrics-core" % "3.2.6" excludeAll (excludeSlf4j: _*)
 
   private val jacksonFamily =
     libraryFamily("com.fasterxml.jackson.core", Versions.JacksonCore)(
@@ -174,7 +183,7 @@ object Dependencies {
       "javax.xml.bind" % "jaxb-api" % "2.3.1",
       h2,
       "com.jolbox" % "bonecp" % "0.8.0.RELEASE",
-      "com.lmax" % "disruptor" % "3.3.6",
+      "com.lmax" % "disruptor" % Versions.Disruptor,
       "com.novocode" % "junit-interface" % Versions.JUnitInterface,
       typesafeConfig,
       sslConfig,
@@ -260,12 +269,12 @@ object Dependencies {
       "org.codehaus.mojo" % "animal-sniffer-annotations" % "1.17",
       "org.hibernate" % "hibernate-validator" % "5.2.4.Final",
       "org.hibernate.javax.persistence" % "hibernate-jpa-2.1-api" % "1.0.0.Final",
-      "org.immutables" % "value" % "2.3.2",
+      "org.immutables" % "value" % Versions.Immutables,
       javassist,
       "org.joda" % "joda-convert" % "1.9.2",
       "org.hamcrest" % "hamcrest-core" % "1.3",
       "org.lmdbjava" % "lmdbjava" % "0.6.1",
-      "org.pcollections" % "pcollections" % "2.1.2",
+      pcollections,
       reactiveStreams,
       "org.scalactic" %% "scalactic" % Versions.ScalaTest,
       scalaTest,
@@ -277,10 +286,10 @@ object Dependencies {
       "org.xerial.snappy" % "snappy-java" % "1.1.7.1",
       "tyrex" % "tyrex" % "1.0.1",
 
-      "org.scala-lang.modules"%%"scala-collection-compat"%"0.1.1",
-      "com.google.guava"%"failureaccess"%"1.0",
-      "com.google.guava"%"listenablefuture"%"9999.0-empty-to-avoid-conflict-with-guava",
-      "javax.activation"%"javax.activation-api"%"1.2.0"
+      "org.scala-lang.modules" %% "scala-collection-compat" % "0.1.1",
+      "com.google.guava" % "failureaccess" % "1.0",
+      "com.google.guava" % "listenablefuture" % "9999.0-empty-to-avoid-conflict-with-guava",
+      "javax.activation" % "javax.activation-api" % "1.2.0"
 
 
     ) ++ jacksonFamily ++ crossLibraryFamily("com.typesafe.akka", Versions.Akka)(
@@ -327,7 +336,7 @@ object Dependencies {
     "javax.el" % "el-api" % "2.2",
     "javax.enterprise" % "cdi-api" % "1.1",
     "org.apache.geronimo.specs" % "geronimo-jta_1.1_spec" % "1.1.1",
-    "org.hibernate" % "hibernate-core" % "5.2.5.Final",
+    "org.hibernate" % "hibernate-core" % Versions.HibernateCore,
     "org.hibernate.common" % "hibernate-commons-annotations" % "5.0.1.Final",
     "org.jboss" % "jandex" % "2.0.3.Final",
     "org.jboss.logging" % "jboss-logging" % "3.3.0.Final",
@@ -378,7 +387,7 @@ object Dependencies {
   val `api-javadsl` = libraryDependencies ++= Seq(
     playJava,
     playGuice,
-    "org.pcollections" % "pcollections" % "2.1.2",
+    pcollections,
 
     // Upgrades needed to match whitelist
     sslConfig,
@@ -394,7 +403,7 @@ object Dependencies {
     scalaTest % Test
   )
 
-  val immutables = libraryDependencies += "org.immutables" % "value" % "2.3.2"
+  val immutables = libraryDependencies += "org.immutables" % "value" % Versions.Immutables
 
   val jackson = libraryDependencies ++= Seq(
     "com.fasterxml.jackson.module" % "jackson-module-parameter-names" % Versions.Jackson,
@@ -405,6 +414,7 @@ object Dependencies {
 
     // Upgrades needed to match whitelist
     sslConfig,
+    pcollections,
 
     akkaTestkit % Test,
     scalaTest % Test,
@@ -421,6 +431,7 @@ object Dependencies {
     // Upgrades needed to match whitelist
     scalaOrganization.value % "scala-reflect" % scalaVersion.value,
     scalaJava8Compat,
+    pcollections,
     scalaXml % Test,
     scalaParserCombinators % Test
     // explicitly depend on particular versions of jackson
@@ -627,7 +638,8 @@ object Dependencies {
   val `persistence-testkit` = libraryDependencies ++= Seq(
     akkaPersistence,
     akkaTestkit,
-    slf4jApi
+    slf4jApi,
+    scalaJava8Compat
   )
 
   val `persistence-javadsl` = libraryDependencies ++= Seq(
@@ -682,8 +694,8 @@ object Dependencies {
   )
 
   val `persistence-jpa-javadsl` = libraryDependencies ++= Seq(
-    "org.hibernate.javax.persistence" % "hibernate-jpa-2.1-api" % "1.0.0.Final" % Provided,
-    "org.hibernate" % "hibernate-core" % "5.2.5.Final" % Test,
+    "org.hibernate.javax.persistence" % "hibernate-jpa-2.1-api" % "1.0.2" % Provided,
+    "org.hibernate" % "hibernate-core" % Versions.HibernateCore % Test,
     h2 % Test,
     javassist % Test
   )
@@ -742,13 +754,14 @@ object Dependencies {
     akkaStream,
     akkaActor,
     akkaSlf4j,
-    akkaProfobuf
+    akkaProfobuf,
+    scalaXml
   ) ++ Seq("logback-core", "logback-classic").map("ch.qos.logback" % _ % Versions.Logback)
 
   val log4j2 = libraryDependencies ++= Seq(slf4jApi) ++
     log4jModules ++
     Seq(
-      "com.lmax" % "disruptor" % "3.3.6",
+      "com.lmax" % "disruptor" % Versions.Disruptor,
       play,
 
       // Upgrades needed to match whitelist versions
@@ -872,8 +885,8 @@ object Dependencies {
     // Note that curator 3.x is only compatible with zookeeper 3.5.x. Kafka currently uses zookeeper 3.4, hence we have
     // to use curator 2.x, which is compatible with zookeeper 3.4 (see the notice in
     // http://curator.apache.org/index.html - make sure to scroll to the bottom)
-    "org.apache.curator" % "curator-framework" % "2.10.0",
-    "org.apache.curator" % "curator-test" % "2.10.0",
+    "org.apache.curator" % "curator-framework" % Versions.Curator,
+    "org.apache.curator" % "curator-test" % Versions.Curator,
     javassist,
     scalaJava8Compat,
     scalaTest % Test
