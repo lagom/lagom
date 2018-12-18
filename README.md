bac6a8dd52a1829682db0f5b1b650b9668a9fe95
Renato Cavalcanti <renato@cavalcanti.be>
2018-12-18 09:11:57 +0100

    Clean up project (#12)
    Removes bootstrap and health-check

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # Akka Discovery based ServiceLocator
 
-This project provides a few components to facilitate the deployment of a Lagom application. 
+This project provides a Lagom `ServiceLocator` implementation based on Akka Discovery. 
+
+# Usage 
 
 It provides:
 
 * A `AkkaDiscoveryServiceLocator` implementation based on [Akka Discovery](https://developer.lightbend.com/docs/akka-management/current/discovery/index.html)
-* A `AkkaDiscoveryServiceLocatorModule` Guice module to wire-up the `AkkaDiscoveryServiceLocator` and bootstrap the Akka Cluster.
-* A `akka-management` `ManagementRouteProvider` extension implementation that can be used for health checks bound to Akka Cluster formation.
-* A [sample application](https://github.com/lagom/service-locator-akka-discovery/tree/master/sample/hello-kubernetes-java-mvn) to demonstrate its usage.
+* A `AkkaDiscoveryServiceLocatorModule` Guice module to wire-up the `AkkaDiscoveryServiceLocator` when using runtime-time DI.
+* A `AkkaDiscoveryComponents` cake component to wire-up the `AkkaDiscoveryServiceLocator` when using compile-time DI (macwire).
 
 This is an experimental project. API and configuration may change.
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,19 +7,13 @@ object Dependencies {
     val Lagom14 = "1.4.9"
     val AkkaManagement = "0.19.0"
     val ScalaTest = "3.0.5"
-    val Play26 = "2.6.20"
   }
 
   val akkaDiscovery = "com.lightbend.akka.discovery" %% "akka-discovery" % Versions.AkkaManagement
-  val akkaClusterHttp = "com.lightbend.akka.management" %% "akka-management-cluster-http" % Versions.AkkaManagement
-  val akkaClusterBootstrap = "com.lightbend.akka.management" %% "akka-management-cluster-bootstrap" % Versions.AkkaManagement
-  val akkaManagement = "com.lightbend.akka.management" %% "akka-management" % Versions.AkkaManagement
 
   val slf4j = "org.slf4j" % "slf4j-api" % "1.7.25" % Provided
   val lagomJavadslClient = "com.lightbend.lagom" %% "lagom-javadsl-client" % Versions.Lagom14 % Provided
   val lagomScaladslClient = "com.lightbend.lagom" %% "lagom-scaladsl-client" % Versions.Lagom14 % Provided
-
-  val play = "com.typesafe.play" %% "play" % Versions.Play26 % Provided
 
   val scalaTest = "org.scalatest" %% "scalatest" % Versions.ScalaTest % Test
   val logback = "ch.qos.logback" % "logback-classic" % "1.2.3" % Test
@@ -37,13 +31,6 @@ object Dependencies {
 
   val serviceLocatorScaladsl = Seq(
     lagomScaladslClient
-  )
-
-  val bootstrap = Seq(
-    akkaManagement,
-    akkaClusterHttp,
-    akkaClusterBootstrap,
-    play
   )
 
 }
