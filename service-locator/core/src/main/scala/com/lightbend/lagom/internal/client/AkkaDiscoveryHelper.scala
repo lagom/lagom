3b5a13f93e9946e1f6ddef90d38e13188629c81e
Renato Cavalcanti <renato@cavalcanti.be>
2018-12-18 12:29:07 +0100

    configure scalafmt (#15)

--- service-locator/core/src/main/scala/com/lightbend/lagom/internal/client/AkkaDiscoveryHelper.scala
+++ service-locator/core/src/main/scala/com/lightbend/lagom/internal/client/AkkaDiscoveryHelper.scala
@@ -1,37 +1,46 @@
 package com.lightbend.lagom.internal.client
 
-import java.net.{URI, URISyntaxException}
-import java.util.concurrent.{ThreadLocalRandom, TimeUnit}
+import java.net.URI
+import java.net.URISyntaxException
+import java.util.concurrent.ThreadLocalRandom
+import java.util.concurrent.TimeUnit
 
-import akka.discovery.{Lookup, SimpleServiceDiscovery}
+import akka.discovery.Lookup
+import akka.discovery.SimpleServiceDiscovery
 import akka.discovery.SimpleServiceDiscovery.ResolvedTarget
 import com.typesafe.config.Config
 import org.slf4j.LoggerFactory
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.collection.JavaConverters._
 
 /**
   * Helper for implementing Akka Discovery based service locators in Lagom.
   */
-private[lagom] class AkkaDiscoveryHelper(config: Config, serviceDiscovery: SimpleServiceDiscovery)(implicit ec: ExecutionContext) {
+private[lagom] class AkkaDiscoveryHelper(config: Config, serviceDiscovery: SimpleServiceDiscovery)(
+    implicit ec: ExecutionContext) {
   private val logger = LoggerFactory.getLogger(this.getClass)
 
   private val serviceNameParser = new ServiceNameParser(config)
   private val lookupTimeout = config.getDuration("lookup-timeout", TimeUnit.MILLISECONDS).millis
   private val portNameSchemeMapping = {
     val mappings = config.getConfig("port-name-scheme-mapping")
-    config.getObject("port-name-scheme-mapping").asScala
+    config
+      .getObject("port-name-scheme-mapping")
+      .asScala
       .map {
         case (key, value) => key -> mappings.getString(key)
-      }.toMap
+      }
+      .toMap
   }
   private val defaultScheme = Some(config.getString("defaults.scheme")).filter(_.nonEmpty)
 
   def locateAll(name: String): Future[Seq[URI]] = {
     val lookupQuery = serviceNameParser.parseLookupQuery(name)
-    serviceDiscovery.lookup(lookupQuery, lookupTimeout)
+    serviceDiscovery
+      .lookup(lookupQuery, lookupTimeout)
       .map { resolved =>
         logger.debug("Retrieved addresses: {}", resolved.addresses)
         resolved.addresses.map(target => toURI(target, lookupQuery))
@@ -44,18 +53,19 @@ private[lagom] class AkkaDiscoveryHelper(config: Config, serviceDiscovery: Simpl
     // it's safe to call 'get' here, those have already been validated in #filterValid
     val port = resolvedTarget.port.getOrElse(-1)
 
-    val scheme = lookup.portName.flatMap(portNameSchemeMapping.get)
+    val scheme = lookup.portName
+      .flatMap(portNameSchemeMapping.get)
       .orElse(defaultScheme)
       .orNull
 
     try {
       new URI(scheme, // scheme
-        null, // userInfo
-        resolvedTarget.host, // host
-        port, // port
-        null, // path
-        null, // query
-        null // fragment
+              null, // userInfo
+              resolvedTarget.host, // host
+              port, // port
+              null, // path
+              null, // query
+              null // fragment
       )
     } catch {
       case e: URISyntaxException => throw new RuntimeException(e)
@@ -63,9 +73,9 @@ private[lagom] class AkkaDiscoveryHelper(config: Config, serviceDiscovery: Simpl
   }
 
   private def selectRandomURI(uris: Seq[URI]) = uris match {
-    case Nil => None
+    case Nil      => None
     case Seq(one) => Some(one)
-    case many => Some(many(ThreadLocalRandom.current().nextInt(many.size)))
+    case many     => Some(many(ThreadLocalRandom.current().nextInt(many.size)))
   }
 
 }
