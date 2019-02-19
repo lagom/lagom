e4ecd8f2a1c8365614d73de61bee78674181dda3
Ignasi Marimon-Clos <ignasi@lightbend.com>
2019-02-19 12:43:55 +0100

    Adds a java complement to the unit tests so we can embed the code in theh docs

--- testkit/javadsl/src/test/java/com/lightbend/lagom/javadsl/testkit/services/tls/TestTlsService.java
+++ testkit/javadsl/src/test/java/com/lightbend/lagom/javadsl/testkit/services/tls/TestTlsService.java
@@ -2,7 +2,7 @@
  * Copyright (C) 2016-2019 Lightbend Inc. <https://www.lightbend.com>
  */
 
-package com.lightbend.lagom.javadsl.testkit;
+package com.lightbend.lagom.javadsl.testkit.services.tls;
 
 import akka.NotUsed;
 import com.lightbend.lagom.javadsl.api.Descriptor;
--- testkit/javadsl/src/test/scala/com/lightbend/lagom/javadsl/testkit/TestOverTlsSpec.scala
+++ testkit/javadsl/src/test/scala/com/lightbend/lagom/javadsl/testkit/TestOverTlsSpec.scala
@@ -14,6 +14,7 @@ import com.google.inject.AbstractModule
 import com.lightbend.lagom.javadsl.api.ServiceCall
 import com.lightbend.lagom.javadsl.server.ServiceGuiceSupport
 import com.lightbend.lagom.javadsl.testkit.ServiceTest.{ Setup, TestServer }
+import com.lightbend.lagom.javadsl.testkit.services.tls.TestTlsService
 import com.typesafe.config.ConfigFactory
 import javax.inject.Inject
 import javax.net.ssl.SSLContext
