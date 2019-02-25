a3149c328bdcb211179bb9e3cf3aee4ea4bda217
Renato Cavalcanti <renato@cavalcanti.be>
2019-02-25 17:43:04 +0100

    Add logging to AkkaManagementTrigger (#1757)
    * Added logging on AkkaManagementTrigger
    
    * formatting
    
    * Fix typo
    
    Co-Authored-By: renatocaval <renato@cavalcanti.be>

--- akka-management/core/src/main/scala/com/lightbend/lagom/internal/akka/management/AkkaManagementTrigger.scala
+++ akka-management/core/src/main/scala/com/lightbend/lagom/internal/akka/management/AkkaManagementTrigger.scala
@@ -8,6 +8,7 @@ import akka.Done
 import akka.actor.{ ActorSystem, CoordinatedShutdown, ExtendedActorSystem }
 import akka.management.scaladsl.AkkaManagement
 import com.typesafe.config.Config
+import play.api.Logger
 
 import scala.concurrent.{ ExecutionContext, Future }
 
@@ -26,7 +27,11 @@ private[lagom] class AkkaManagementTrigger(
   coordinatedShutdown: CoordinatedShutdown
 )(implicit executionContext: ExecutionContext) {
 
-  private val enabled = config.getBoolean("lagom.akka.management.enabled")
+  private val enabledSetting = "lagom.akka.management.enabled"
+  private val enabledRenderedValue = config.getValue(enabledSetting).render()
+  private val enabled = config.getBoolean(enabledSetting)
+
+  private val logger = Logger(this.getClass)
 
   /**
    * Starts Akka HTTP Management honoring the `lagom.akka.management.enabled` setting
@@ -34,9 +39,11 @@ private[lagom] class AkkaManagementTrigger(
   private[lagom] def start() = {
 
     if (enabled) {
-      forcedStart()
+      doStart()
     } else {
-      // TODO: trace a WARN to state nothing happened
+      logger.debug(
+        s"'lagom.akka.management.enabled' property is set to '$enabledRenderedValue'. Akka Management won't start."
+      )
       Future.successful(Done)
     }
 
@@ -45,11 +52,19 @@ private[lagom] class AkkaManagementTrigger(
   /**
    * Starts Akka HTTP Management ignoring the `lagom.akka.management.enabled` setting.
    */
-  private[lagom] def forcedStart(): Future[Done] = {
+  private[lagom] def forcedStart(requester: String): Future[Done] = {
 
     if (!enabled) {
-      // TODO: trace that flag is ignored and AHM is started anyway
+      logger.warn(
+        s"'lagom.akka.management.enabled' property is set to '$enabledRenderedValue', " +
+          s"but Akka Management is being required to start by: '$requester'."
+      )
     }
+
+    doStart()
+  }
+
+  private def doStart(): Future[Done] = {
 
     val akkaManagement = AkkaManagement(system.asInstanceOf[ExtendedActorSystem])
     akkaManagement.start().map {
@@ -63,6 +78,5 @@ private[lagom] class AkkaManagementTrigger(
           }
         Done
     }
-
   }
 }
