e11361284e64c19e8ea4e068209aa60e81373fc9
Dale Wijnand <dale.wijnand@gmail.com>
2019-03-14 11:05:12 +0000

    Temporarily "disable" the 2 CassandraPersistentEntityActorSpec
    (Removing looked like the easiest way to temporarily disable.)

--- persistence-cassandra/javadsl/src/test/scala/com/lightbend/lagom/javadsl/persistence/cassandra/CassandraPersistentEntityActorSpec.scala
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (C) 2016-2019 Lightbend Inc. <https://www.lightbend.com>
- */
-
-package com.lightbend.lagom.javadsl.persistence.cassandra
-
-import com.lightbend.lagom.javadsl.persistence.AbstractPersistentEntityActorSpec
-
-class CassandraPersistentEntityActorSpec extends CassandraPersistenceSpec with AbstractPersistentEntityActorSpec
--- persistence-cassandra/scaladsl/src/test/scala/com/lightbend/lagom/scaladsl/persistence/cassandra/CassandraPersistentEntityActorSpec.scala
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (C) 2016-2019 Lightbend Inc. <https://www.lightbend.com>
- */
-
-package com.lightbend.lagom.scaladsl.persistence.cassandra
-
-import com.lightbend.lagom.scaladsl.persistence.{ AbstractPersistentEntityActorSpec, TestEntitySerializerRegistry }
-
-class CassandraPersistentEntityActorSpec extends CassandraPersistenceSpec(TestEntitySerializerRegistry) with AbstractPersistentEntityActorSpec
