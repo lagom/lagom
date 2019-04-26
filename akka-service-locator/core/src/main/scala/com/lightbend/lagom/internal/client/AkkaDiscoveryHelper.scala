f3c7ef671086b890716531ced896607707d273d9
Renato Cavalcanti <renato@cavalcanti.be>
2019-04-26 14:27:15 +0200

    formatting

--- akka-service-locator/core/src/main/scala/com/lightbend/lagom/internal/client/AkkaDiscoveryHelper.scala
+++ akka-service-locator/core/src/main/scala/com/lightbend/lagom/internal/client/AkkaDiscoveryHelper.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018-2019 Lightbend Inc. <https://www.lightbend.com>
+ * Copyright (C) 2016-2019 Lightbend Inc. <https://www.lightbend.com>
  */
 
 package com.lightbend.lagom.internal.client
@@ -19,10 +19,12 @@ import scala.concurrent.Future
 import scala.concurrent.duration._
 
 /**
-  * Helper for implementing Akka Discovery based service locators in Lagom.
-  */
+ * Helper for implementing Akka Discovery based service locators in Lagom.
+ */
 private[lagom] class AkkaDiscoveryHelper(config: Config, serviceDiscovery: ServiceDiscovery)(
-    implicit ec: ExecutionContext) {
+  implicit
+  ec: ExecutionContext
+) {
 
   private val logger = LoggerFactory.getLogger(this.getClass)
 
@@ -48,13 +50,14 @@ private[lagom] class AkkaDiscoveryHelper(config: Config, serviceDiscovery: Servi
     val scheme = lookup.scheme.orNull
 
     try {
-      new URI(scheme, // scheme
-              null, // userInfo
-              resolvedTarget.host, // host
-              port, // port
-              null, // path
-              null, // query
-              null // fragment
+      new URI(
+        scheme, // scheme
+        null, // userInfo
+        resolvedTarget.host, // host
+        port, // port
+        null, // path
+        null, // query
+        null // fragment
       )
     } catch {
       case e: URISyntaxException => throw new RuntimeException(e)
