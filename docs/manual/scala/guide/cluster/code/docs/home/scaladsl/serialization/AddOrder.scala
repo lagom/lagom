78c3c0f01ee3dd022b35fe7edcfbb4b2fada9de0
Ignasi Marimon-Clos <ignasi@lightbend.com>
2019-05-03 16:33:04 +0200

    Disable formatter on some doc snippets

--- docs/manual/scala/guide/cluster/code/docs/home/scaladsl/serialization/AddOrder.scala
+++ docs/manual/scala/guide/cluster/code/docs/home/scaladsl/serialization/AddOrder.scala
@@ -11,6 +11,7 @@ import play.api.libs.json.Reads
 
 object AddOrder {
 
+  // format: off
   //#manualMapping
   case class AddOrder(productId: String, quantity: Int)
 
@@ -18,11 +19,13 @@ object AddOrder {
   import play.api.libs.json._
 
   object AddOrder {
-    implicit val format: Format[AddOrder] =
-      (JsPath \ "product_id").format[String].and((JsPath \ "quantity").format[Int])
-    (AddOrder.apply, unlift(AddOrder.unapply))
+    implicit val format: Format[AddOrder] = (
+        (JsPath \ "product_id").format[String] and
+          (JsPath \ "quantity").format[Int]
+        ) (AddOrder.apply, unlift(AddOrder.unapply))
   }
   //#manualMapping
+  // format: on
 
 }
 
