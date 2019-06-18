df84c104b5679f870cf79a747b9086f2c84b5dd0
Patrik Nordwall <patrik.nordwall@gmail.com>
2019-06-18 08:14:05 +0200

    Remove OldJacksonJsonSerializer, not needed
    * bind old serializerId to an instance of Akka's JacksonJsonSerializer
    * move migration config check to JacksonModule

--- jackson/src/main/scala/com/lightbend/lagom/javadsl/jackson/JacksonModule.scala
+++ jackson/src/main/scala/com/lightbend/lagom/javadsl/jackson/JacksonModule.scala
@@ -4,17 +4,31 @@
 
 package com.lightbend.lagom.javadsl.jackson
 
+import akka.actor.ActorSystem
+import akka.serialization.jackson.JacksonMigration
 import play.api.Configuration
 import play.api.Environment
 import play.api.inject.Binding
 import play.api.inject.Module
+import javax.inject.Inject
 
 /**
  * Module that provides the default Jackson serializer factory.
  */
 class JacksonModule extends Module {
   override def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] = Seq(
+    bind[JacksonMigrationCheck].toSelf.eagerly(),
     bind[JacksonSerializerFactory].toSelf,
     bind[JacksonExceptionSerializer].toSelf
   )
+}
+
+private[lagom] class JacksonMigrationCheck @Inject()(system: ActorSystem) {
+  if (system.settings.config.hasPath("lagom.serialization.json.migrations")) {
+    throw new IllegalStateException(
+      "JacksonJsonSerializer migrations defined in " +
+        s"'lagom.serialization.json.migrations' must be rewritten as [${classOf[JacksonMigration].getName}] " +
+        "and defined in config 'akka.serialization.jackson.migrations'."
+    )
+  }
 }
