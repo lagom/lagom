08ea15f1b363bac677cbf712b32a72f53d9bdd72
Ignasi Marimon-Clos <ignasi@lightbend.com>
2019-07-04 19:38:29 +0200

    An Initial test covering basic ClusterDistrib

--- persistence/core/src/multi-jvm/scala/com/lightbend/lagom/internal/persistence/cluster/ClusterDistributionSpec.scala
+++ persistence/core/src/multi-jvm/scala/com/lightbend/lagom/internal/persistence/cluster/ClusterDistributionSpec.scala
@@ -4,21 +4,27 @@
 
 package com.lightbend.lagom.internal.persistence.cluster
 
+import akka.actor.ActorRef
 import akka.actor.ActorSystem
+import akka.actor.Address
 import akka.actor.BootstrapSetup
 import akka.actor.Props
 import akka.actor.setup.ActorSystemSetup
+import akka.cluster.Cluster
+import akka.cluster.MemberStatus
 import akka.cluster.sharding.ClusterShardingSettings
+import akka.remote.testconductor.RoleName
 import akka.remote.testkit.MultiNodeConfig
 import akka.remote.testkit.MultiNodeSpec
 import akka.testkit.ImplicitSender
+import akka.testkit.TestProbe
 import com.lightbend.lagom.internal.persistence.cluster.ClusterDistribution.EnsureActive
 import com.lightbend.lagom.scaladsl.persistence.multinode.STMultiNodeSpec
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import com.typesafe.config.ConfigFactory
 
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration._
 
 object ClusterDistributionConfig extends MultiNodeConfig {
   val node1 = role("node1")
@@ -53,19 +59,47 @@ class ClusterDistributionSpec
     with STMultiNodeSpec
     with ImplicitSender {
 
-  import ClusterDistributionConfig._
+import ClusterDistributionConfig._
   override def initialParticipants: Int = roles.size
+
+
+  def join(from: RoleName, to: RoleName): Unit = {
+    runOn(from) {
+      Cluster(system).join(node(to).address)
+    }
+    enterBarrier(from.name + "-joined")
+  }
+
+  def fullAddress(ref: ActorRef): Address =
+    if (ref.path.address.hasLocalScope) Cluster(system).selfAddress
+    else ref.path.address
+
+  protected override def atStartup() {
+    roles.foreach(n => join(n, node1))
+    within(15.seconds) {
+      awaitAssert(Cluster(system).state.members.size should be(3))
+      awaitAssert(
+        Cluster(system).state.members.toIndexedSeq.map(_.status).distinct should be(IndexedSeq(MemberStatus.Up))
+      )
+    }
+
+    enterBarrier("startup")
+  }
+
 
   val distributionSettings =
     ClusterDistributionSettings(system)
-      .copy(ensureActiveInterval = Duration(1, "second"))
+      .copy(ensureActiveInterval = 1.second)
 
   "A ClusterDistribution" must {
 
     "distribute" in {
+
+      val probe = TestProbe()
+
       val typeName     = "CDTest"
-      val props: Props = FakeActor.props
-      val tagNames     = (1 to 10).map(i => s"test$i").toSet
+      val props: Props = FakeActor.props(probe.ref)
+      val tagNames     = (1 to 10).map(i => s"test-tagName$i").toSet
       ClusterDistribution(system)
         .start(
           typeName,
@@ -73,19 +107,22 @@ class ClusterDistributionSpec
           tagNames,
           distributionSettings
         )
+
+      val echoMessage = probe.receiveOne(10.second).asInstanceOf[String]
+      echoMessage should include ("test-tagName")
+
     }
   }
 }
 
-import akka.actor.Actor
-import akka.actor.Props
+import akka.actor.{ Actor, Props }
 
 object FakeActor {
-  def props: Props = Props(new FakeActor)
+  def props(ref:ActorRef): Props = Props(new FakeActor(ref))
 }
 
-class FakeActor extends Actor {
+class FakeActor(ref:ActorRef) extends Actor {
   override def receive = {
-    case EnsureActive(tagName) => println(tagName)
+    case EnsureActive(tagName) => ref ! tagName
   }
 }
