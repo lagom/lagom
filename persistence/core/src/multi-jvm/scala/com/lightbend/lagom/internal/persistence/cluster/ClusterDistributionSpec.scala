37799caa0e0848e4a4ebf58abc9bd3dac1b71c08
Ignasi Marimon-Clos <ignasi@lightbend.com>
2019-07-05 12:19:55 +0200

    Improves the test

--- persistence/core/src/multi-jvm/scala/com/lightbend/lagom/internal/persistence/cluster/ClusterDistributionSpec.scala
+++ persistence/core/src/multi-jvm/scala/com/lightbend/lagom/internal/persistence/cluster/ClusterDistributionSpec.scala
@@ -30,21 +30,26 @@ object ClusterDistributionConfig extends MultiNodeConfig {
   val node3 = role("node3")
 
   commonConfig(
-      ConfigFactory
-        .parseString(
-          """
+    ConfigFactory
+      .parseString(
+        """
+      akka.actor.provider = cluster
       terminate-system-after-member-removed = 60s
 
       # increase default timeouts to leave wider margin for Travis.
       # 30s to 60s
       akka.testconductor.barrier-timeout=60s
-
       akka.test.single-expect-default = 15s
+
+      akka.cluster.sharding.waiting-for-state-timeout = 5s
 
       # Don't terminate the actor system when doing a coordinated shutdown
       akka.coordinated-shutdown.terminate-actor-system = off
       akka.coordinated-shutdown.run-by-jvm-shutdown-hook = off
       akka.cluster.run-coordinated-shutdown-when-down = off
+
+      ## The settings below are incidental because this code lives in a project that depends on lagom-cluster and
+      ## lagom-akka-management-core.
 
       # multi-jvm tests forms the cluster programmatically
       # therefore we disable Akka Cluster Bootstrap
@@ -52,12 +57,9 @@ object ClusterDistributionConfig extends MultiNodeConfig {
 
       # no jvm exit on tests
       lagom.cluster.exit-jvm-when-system-terminated = off
-
-      akka.cluster.sharding.waiting-for-state-timeout = 5s
     """
-        )
-        .withFallback(ConfigFactory.parseResources("play/reference-overrides.conf"))
-    )
+      )
+  )
 
 }
 
@@ -72,8 +74,8 @@ object ClusterDistributionSpec {
     }
     reduced.head.replaceFirst(""".*\.""", "").replaceAll("[^a-zA-Z_0-9]", "_")
   }
-  def createActorSystem(): (Config) => ActorSystem = { config =>
-    val setup      = ActorSystemSetup(BootstrapSetup(ConfigFactory.load(config)))
+  def createActorSystem(): Config => ActorSystem = { config =>
+    val setup = ActorSystemSetup(BootstrapSetup(ConfigFactory.load(config)))
     ActorSystem(getCallerName(classOf[MultiNodeSpec]), setup)
   }
 }
@@ -88,6 +90,8 @@ class ClusterDistributionSpec
     with ImplicitSender {
 
   import ClusterDistributionConfig._
+
+  //  -------- A first piece of testing scaffolding and cluster formation  ------------------
   override def initialParticipants: Int = roles.size
 
   def join(from: RoleName, to: RoleName): Unit = {
@@ -115,9 +119,10 @@ class ClusterDistributionSpec
 
   //  -------- Before this line all code is testing scaffolding or cluster formation  ------------------
 
+  private val ensureActiveInterval: FiniteDuration = 1.second
   val distributionSettings =
     ClusterDistributionSettings(system)
-      .copy(ensureActiveInterval = 1.second)
+      .copy(ensureActiveInterval = ensureActiveInterval)
 
   "A ClusterDistribution" must {
 
@@ -125,25 +130,38 @@ class ClusterDistributionSpec
 
       val probe = TestProbe()
 
-      val typeName     = "CDTest"
+      val typeName = "CDTest"
       // There'll be 3 nodes in this test and each node will host a TestProbe.
       // Cluster Distribution on each node will create a `FakeActor.props` pointing
       // back to its own TestProbe. We request the creation of 10 FakeActor to ClusterDistribution
-      // with the hope that there'll be at least one for each TestProbe. This is flaky but unlikely to fail.
-      // If it failed, just increase the number of entityIds.
+      // with the expectation that there'll be 3 or 4 FakeActor's for each TestProbe.
       val props: Props = FakeActor.props(probe.ref)
-      val entityIds     = (1 to 10).map(i => s"test-entityId$i").toSet
-      ClusterDistribution(system)
-        .start(
-          typeName,
-          props,
-          entityIds,
-          distributionSettings
-        )
+      val entityIds    = (1 to 10).map(i => s"test-entityId$i").toSet
 
-      val echoMessage = probe.receiveOne(10.second).asInstanceOf[String]
-      echoMessage should include("test-entityId")
+      // Load the extension and wait for other nodes to be ready before proceeding
+      val cdExtension  = ClusterDistribution(system)
+      enterBarrier("extension-is-loaded")
+      cdExtension.start(
+        typeName,
+        props,
+        entityIds,
+        distributionSettings
+      )
 
+      // During 10secs, or when 10 responses are obtained, record responses.
+      // The reported messages are a Tuple2[sender.path, entityId] so the `sender.path` must be 3 different
+      // values (representing each of the TestProbe's).
+      val reportedMessages: Seq[(String, String)] = probe.receiveN(10, 10.second).asInstanceOf[Seq[(String, String)]]
+
+      // Any of the 10 FakeActor instances will receive `EnsureActive` messages from any of the nodes. So, the
+      // reported message must have 3 distinct senders.
+      reportedMessages.map(_._1).distinct.size should be(3)
+
+      // The `entityId`, OTOH, works differently. a TestProbe will only get reports from the FakeActor's
+      // that were created locally so in the 10 reports we'll get always the same 3 or 4 entityId's.
+      // The expected size is 3 <= x <= 4 because evenly distributing 10 shards in 3 nodes gives these numbers.
+      reportedMessages.map(_._2).distinct.size should be >= 3
+      reportedMessages.map(_._2).distinct.size should be <= 4
     }
   }
 }
@@ -152,11 +170,15 @@ import akka.actor.Actor
 import akka.actor.Props
 
 object FakeActor {
-  def props(ref: ActorRef): Props = Props(new FakeActor(ref))
+  def props(creatorRef: ActorRef): Props = Props(new FakeActor(creatorRef))
 }
 
-class FakeActor(ref: ActorRef) extends Actor {
+// This actor keeps a reference to the test instance that created it (this is a multi-node test
+// so there are multiple tests instances).
+// Each node in the cluster may send messages to this actor but this actor will only report back
+// to its creator.
+class FakeActor(creatorRef: ActorRef) extends Actor {
   override def receive = {
-    case EnsureActive(entityId) => ref ! entityId
+    case EnsureActive(entityId) => creatorRef ! (sender.path.address.toString, entityId)
   }
 }
