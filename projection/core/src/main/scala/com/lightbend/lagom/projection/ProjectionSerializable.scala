0267e5a16d82152b83a2a7b5b216a8c7fe006a66
Renato Cavalcanti <renato@cavalcanti.be>
2019-08-01 13:33:50 +0200

    Protobuf serializers for Projection model

---
+++ projection/core/src/main/scala/com/lightbend/lagom/projection/ProjectionSerializable.scala
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2016-2019 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+/** Marker trait for projection serializers */
+ package com.lightbend.lagom.projection
+
+trait ProjectionSerializable
