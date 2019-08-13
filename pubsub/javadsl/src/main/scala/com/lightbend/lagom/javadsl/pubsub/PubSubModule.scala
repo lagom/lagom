b4315b08a1134f7fb7487c8fcd2b67e9c4ffdb76
Marcos Pereira <marcos.silva@gmail.com>
2019-08-13 16:43:51 -0400

    Remove unnecessary module

--- pubsub/javadsl/src/main/scala/com/lightbend/lagom/javadsl/pubsub/PubSubModule.scala
+++ pubsub/javadsl/src/main/scala/com/lightbend/lagom/javadsl/pubsub/PubSubModule.scala
@@ -5,19 +5,12 @@
 package com.lightbend.lagom.javadsl.pubsub
 
 import com.lightbend.lagom.internal.javadsl.pubsub.PubSubRegistryImpl
-import play.api.Configuration
-import play.api.Environment
-import play.api.inject.Binding
-import play.api.inject.Module
+import play.api.inject._
 
 /**
  * Guice module for the PubSub API.
  */
-class PubSubModule extends Module {
-
-  override def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] = Seq(
-    // eagerSingleton because the distributed registry benefits from being alive as early as possible.
-    bind[PubSubRegistry].to[PubSubRegistryImpl].eagerly()
-  )
-
-}
+class PubSubModule extends SimpleModule(
+  // eagerSingleton because the distributed registry benefits from being alive as early as possible.
+  bind[PubSubRegistry].to[PubSubRegistryImpl].eagerly()
+)
--- pubsub/javadsl/src/multi-jvm/scala/com/lightbend/lagom/javadsl/pubsub/ClusteredPubSubSpec.scala
+++ pubsub/javadsl/src/multi-jvm/scala/com/lightbend/lagom/javadsl/pubsub/ClusteredPubSubSpec.scala
@@ -4,6 +4,8 @@
 
 package com.lightbend.lagom.javadsl.pubsub
 
+import akka.actor.ActorSystem
+
 import scala.concurrent.duration._
 import akka.remote.testkit.MultiNodeConfig
 import akka.remote.testkit.MultiNodeSpec
@@ -12,15 +14,15 @@ import com.typesafe.config.ConfigFactory
 import akka.remote.testconductor.RoleName
 import akka.cluster.Cluster
 import akka.stream.ActorMaterializer
+import akka.stream.Materializer
 import akka.stream.scaladsl.Source
 import akka.stream.testkit.scaladsl.TestSink
-import com.lightbend.lagom.javadsl.cluster.testkit.ActorSystemModule
 import play.api.inject.guice.GuiceInjectorBuilder
 import com.lightbend.lagom.internal.cluster.STMultiNodeSpec
-import play.api.inject.ApplicationLifecycle
-import play.api.inject.DefaultApplicationLifecycle
+import play.api.inject._
 
 import scala.concurrent.Await
+import scala.concurrent.ExecutionContext
 
 object ClusteredPubSubConfig extends MultiNodeConfig {
   val node1 = role("node1")
@@ -66,8 +68,13 @@ class ClusteredPubSubSpec extends MultiNodeSpec(ClusteredPubSubConfig) with STMu
   val applicationLifecycle = new DefaultApplicationLifecycle
 
   val injector = new GuiceInjectorBuilder()
-    .bindings(new ActorSystemModule(system), new PubSubModule)
-    .bindings(play.api.inject.bind[ApplicationLifecycle].toInstance(applicationLifecycle))
+    .bindings(new PubSubModule)
+    .bindings(
+      bind[ApplicationLifecycle].toInstance(applicationLifecycle),
+      bind[ActorSystem].toInstance(system),
+      bind[Materializer].toInstance(mat),
+      bind[ExecutionContext].toInstance(system.dispatcher)
+    )
     .build()
 
   val registry = injector.instanceOf(classOf[PubSubRegistry])
