1f45daca04ce9b41fe5208eb39488c9a99522468
Marcos Pereira <marcos.silva@gmail.com>
2019-08-22 01:51:41 -0400

    Fix code format

--- persistence-cassandra/javadsl/src/test/scala/com/lightbend/lagom/javadsl/persistence/cassandra/CassandraPersistentEntityActorSpec.scala
+++ persistence-cassandra/javadsl/src/test/scala/com/lightbend/lagom/javadsl/persistence/cassandra/CassandraPersistentEntityActorSpec.scala
@@ -7,4 +7,6 @@ package com.lightbend.lagom.javadsl.persistence.cassandra
 import com.lightbend.lagom.javadsl.persistence.AbstractPersistentEntityActorSpec
 import com.typesafe.config.ConfigFactory
 
-class CassandraPersistentEntityActorSpec extends CassandraPersistenceSpec(ConfigFactory.parseString("akka.loglevel = INFO")) with AbstractPersistentEntityActorSpec
+class CassandraPersistentEntityActorSpec
+    extends CassandraPersistenceSpec(ConfigFactory.parseString("akka.loglevel = INFO"))
+    with AbstractPersistentEntityActorSpec
