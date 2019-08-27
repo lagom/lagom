7d890e4878a537cd7fdada7625d0aba137e5f0e5
Dale Wijnand <dale.wijnand@gmail.com>
2019-08-27 12:17:02 +0100

    Prevent API/bincompat issues in projections

--- projection/core/src/main/scala/com/lightbend/lagom/projection/State.scala
+++ projection/core/src/main/scala/com/lightbend/lagom/projection/State.scala
@@ -4,6 +4,8 @@
 
 package com.lightbend.lagom.projection
 
+import java.util.Objects
+
 import akka.annotation.ApiMayChange
 import com.lightbend.lagom.internal.projection.ProjectionRegistryActor.WorkerCoordinates
 
@@ -17,14 +19,38 @@ sealed trait Started extends Status
 case object Started  extends Started
 
 @ApiMayChange
-final case class Worker(tagName: String, key: String, requestedStatus: Status, observedStatus: Status)
-    extends ProjectionSerializable
+final class Worker private (
+    val tagName: String,
+    val key: String,
+    val requestedStatus: Status,
+    val observedStatus: Status,
+) extends ProjectionSerializable {
+  override def equals(other: Any): Boolean =
+    this.eq(other.asInstanceOf[AnyRef]) || (other match {
+      case that: Worker =>
+        tagName == that.tagName &&
+          key == that.key &&
+          requestedStatus == that.requestedStatus &&
+          observedStatus == that.observedStatus
+      case _ => false
+    })
+  override def hashCode(): Int = Objects.hash(tagName, key, requestedStatus, observedStatus)
+}
+
+object Worker {
+  def apply(tagName: String, key: String, requestedStatus: Status, observedStatus: Status) =
+    new Worker(tagName, key, requestedStatus, observedStatus)
+}
 
 @ApiMayChange
-final case class Projection(name: String, workers: Seq[Worker]) extends ProjectionSerializable
+final class Projection private (val name: String, val workers: Seq[Worker]) extends ProjectionSerializable
+
+object Projection {
+  def apply(name: String, workers: Seq[Worker]) = new Projection(name, workers)
+}
 
 @ApiMayChange
-final case class State(projections: Seq[Projection]) extends ProjectionSerializable {
+final class State(val projections: Seq[Projection]) extends ProjectionSerializable {
   def findProjection(projectionName: String): Option[Projection] =
     projections.find(_.name == projectionName)
 
@@ -61,16 +87,17 @@ object State {
     val projections = workers.map {
       case (projectionName, ws) => Projection(projectionName, ws)
     }.toSeq
-    State(projections)
+    new State(projections)
   }
 
 }
 
 @ApiMayChange
-case class ProjectionNotFound(projectionName: String)
+final class ProjectionNotFound(val projectionName: String)
     extends RuntimeException(s"Projection $projectionName is not registered")
     with NoStackTrace
+
 @ApiMayChange
-case class ProjectionWorkerNotFound(workerName: String)
+final class ProjectionWorkerNotFound(val workerName: String)
     extends RuntimeException(s"Projection $workerName is not registered")
     with NoStackTrace
