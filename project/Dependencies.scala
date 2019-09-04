d2bc451e36d17d70b62b49eb7d122202ff486270
Enno Runne <458526+ennru@users.noreply.github.com>
2019-09-04 13:13:30 +0200

    Docs: improve links to Alpakka Kafka

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -44,6 +44,7 @@ object Dependencies {
     val Netty                = "4.1.39.Final"
     val NettyReactiveStreams = "2.0.3"
     val Kafka                = "2.1.1"
+    // adapt links in (java/scala)/KafkaClient.md for minor version changes
     val AlpakkaKafka         = "1.0.5"
     val Curator              = "2.12.0"
     val Immutables           = "2.7.5"
