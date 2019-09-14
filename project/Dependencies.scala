ebbbf3daab81d991be6e5fe95e9ed57a5a3d9b14
Scala Steward <me@scala-steward.org>
2019-09-14 08:56:22 +0200

    Update build-link, play, play-ahc-ws, ... to 2.8.0-M5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     val SbtScala = Seq(Scala212, Scala210)
 
     // If you update the version of Play, you probably need to update the other Play* variables.
-    val Play             = "2.8.0-M4"
+    val Play             = "2.8.0-M5"
     val PlayJson         = "2.8.0-M5"
     val PlayStandaloneWs = "2.1.0-M4"
     val Twirl            = "1.5.0-M2"
