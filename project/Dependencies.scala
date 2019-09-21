ae46903eea7c3359be1ab76519dd008c333158d3
Scala Steward <me@scala-steward.org>
2019-09-21 04:23:07 +0200

    Update akka-http, akka-http-core, ... to 10.1.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
     val PlayFileWatch    = "1.1.8"
 
     val Akka: String = sys.props.getOrElse("lagom.build.akka.version", "2.6.0-M5")
-    val AkkaHttp     = "10.1.9"
+    val AkkaHttp     = "10.1.10"
 
     val AkkaPersistenceCassandra = "0.99"
     val AkkaPersistenceJdbc      = "3.5.2"
