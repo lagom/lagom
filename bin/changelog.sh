d8617462f59a9dc3b8f287a9ac985268f474bda0
Dale Wijnand <dale.wijnand@gmail.com>
2019-09-27 11:59:16 +0100

    Fix remaining shellcheck errors & warnings

--- bin/changelog.sh
+++ bin/changelog.sh
@@ -14,11 +14,11 @@
 
 
 FROM=$1
-if [ $2 ]; then
-  TO=$2
+if [ "$2" ]; then
+  TO="$2"
 else
   TO=HEAD
 fi
 
 echo "Changelog from $FROM to $TO"
-  git log --pretty=format:"%s %an" $FROM..$TO | sed 's/\(.*\)(#\([0-9]*\)) \(.*\)/\* [\2](https:\/\/github.com\/lagom\/lagom\/issues\/\2) \1(\3)/'
+  git log --pretty=format:"%s %an" "$FROM".."$TO" | sed 's/\(.*\)(#\([0-9]*\)) \(.*\)/\* [\2](https:\/\/github.com\/lagom\/lagom\/issues\/\2) \1(\3)/'
