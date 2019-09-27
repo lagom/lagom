b626c6da2ccbb185fc814c2ea32cfeb960594b44
Renato Cavalcanti <renato@cavalcanti.be>
2019-09-27 17:40:35 +0200

    added AkkaTaggerAdapter

---
+++ persistence/javadsl/src/main/java/com/lightbend/lagom/javadsl/AkkaTaggerAdapter.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2016-2019 Lightbend Inc. <https://www.lightbend.com>
+ */
+package com.lightbend.lagom.javadsl.persistence;
+
+import akka.cluster.sharding.typed.javadsl.EntityContext;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Function;
+
+public class AkkaTaggerAdapter {
+
+  /**
+   * Adapts an existing Lagom {code AggregateEventTagger} to a function {code Function<Event,
+   * Set<String>>} as expected by Akka Persistence Typed {code EventSourcedBehavior.withTagger} API.
+   */
+  public static <Event extends AggregateEvent<Event>> Function<Event, Set<String>> fromLagom(
+      EntityContext<?> entityContext, AggregateEventTagger<Event> lagomTagger) {
+    return evt -> {
+      Set<String> tags = new HashSet<>();
+      if (lagomTagger instanceof AggregateEventTag) {
+        tags.add(((AggregateEventTag) lagomTagger).tag());
+      } else if (lagomTagger instanceof AggregateEventShards) {
+        tags.add(
+            ((AggregateEventShards) lagomTagger).forEntityId(entityContext.getEntityId()).tag());
+      }
+      return tags;
+    };
+  }
+}
