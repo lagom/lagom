ec76bec6836d63d3db19c4e8fb8d5a883f4b4b2e
Ignasi Marimon-Clos <ignasi@lightbend.com>
2019-09-30 17:15:33 +0200

    Add ClusterSharding Typed under the covers to every lagom cluster usage

---
+++ cluster/scaladsl/src/main/scala/com/lightbend/lagom/scaladsl/cluster/typed/ClusterShardingTypedComponents.scala
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2016-2019 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package com.lightbend.lagom.scaladsl.cluster.typed
+
+import akka.actor.{ ActorSystem => ActorSystemClassic }
+import akka.cluster.{ Cluster => ClusterClassic }
+import akka.actor.typed.ActorSystem
+import com.lightbend.lagom.scaladsl.cluster.ClusterComponents
+import akka.cluster.sharding.typed.scaladsl.ClusterSharding
+
+/**
+ * Akka Cluster Sharding Typed components (for compile-time injection).
+ */
+trait ClusterShardingTypedComponents  {
+  def actorSystem: ActorSystemClassic
+
+  lazy val clusterShardingTyped: ClusterSharding = {
+    val actorSystemTyped: ActorSystem[_] = {
+      import akka.actor.typed.scaladsl.adapter._
+      actorSystem.toTyped
+    }
+    ClusterSharding(actorSystemTyped)
+  }
+}
