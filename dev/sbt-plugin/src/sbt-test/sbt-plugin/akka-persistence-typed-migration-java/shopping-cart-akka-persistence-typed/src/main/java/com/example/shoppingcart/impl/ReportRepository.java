abd3572af4e0ecba9b2cdc0c6fbe53320e44b944
Renato Cavalcanti <renato@cavalcanti.be>
2019-10-11 17:12:26 +0200

    test migration to akka persistence typed (java)

---
+++ dev/sbt-plugin/src/sbt-test/sbt-plugin/akka-persistence-typed-migration-java/shopping-cart-akka-persistence-typed/src/main/java/com/example/shoppingcart/impl/ReportRepository.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2016-2019 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package com.example.shoppingcart.impl;
+
+import com.lightbend.lagom.javadsl.persistence.ReadSide;
+import com.lightbend.lagom.javadsl.persistence.jpa.JpaSession;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.concurrent.CompletionStage;
+
+@Singleton
+public class ReportRepository {
+
+    private final JpaSession jpaSession;
+
+    @Inject
+    public ReportRepository(ReadSide readSide, JpaSession jpaSession) {
+        this.jpaSession = jpaSession;
+        readSide.register(ShoppingCartReportProcessor.class);
+    }
+
+    CompletionStage<ShoppingCartReport> findById(String cartId) {
+        return jpaSession.withTransaction(em -> em.find(ShoppingCartReport.class, cartId));
+    }
+
+}
---
+++ dev/sbt-plugin/src/sbt-test/sbt-plugin/akka-persistence-typed-migration-java/shopping-cart-akka-persistence-typed/src/main/java/com/example/shoppingcart/impl/ShoppingCartReport.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2016-2019 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package com.example.shoppingcart.impl;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class ShoppingCartReport {
+
+    @Id
+    private String id;
+
+    @NotNull
+    private boolean created;
+
+    private boolean checkedOut;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @NotNull
+    public boolean isCreated() {
+        return created;
+    }
+
+    public void setCreated(@NotNull boolean created) {
+        this.created = created;
+    }
+
+    public boolean isCheckedOut() {
+        return checkedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        this.checkedOut = checkedOut;
+    }
+}
