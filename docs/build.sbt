68bcf9ac4fbc39a8194da7e3d4ff0c6a0b7e24bb
Scala Steward <me@scala-steward.org>
2019-10-29 14:22:14 +0100

    Update akka-actor, ... to 2.6.0-RC2

--- docs/build.sbt
+++ docs/build.sbt
@@ -3,7 +3,7 @@ import akka.CrossJava
 
 val ScalaVersion = "2.12.10"
 
-val AkkaVersion = sys.props.getOrElse("lagom.build.akka.version", "2.6.0-M8") // sync with project/Dependencies.scala
+val AkkaVersion = sys.props.getOrElse("lagom.build.akka.version", "2.6.0-RC2") // sync with project/Dependencies.scala
 
 val JUnitVersion          = "4.12"
 val JUnitInterfaceVersion = "0.11"
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
     val Twirl            = "1.5.0-M4" // sync with docs/project/plugins.sbt
     val PlayFileWatch    = "1.1.8"
 
-    val Akka: String = sys.props.getOrElse("lagom.build.akka.version", "2.6.0-M8") // sync with docs/build.sbt
+    val Akka: String = sys.props.getOrElse("lagom.build.akka.version", "2.6.0-RC2") // sync with docs/build.sbt
     val AkkaHttp     = "10.1.10"
 
     val AkkaPersistenceCassandra = "0.100"
