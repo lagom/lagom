bf995daf53b830c4841a503aac955dcb35f78fc3
Scala Steward <me@scala-steward.org>
2019-11-08 02:26:31 +0100

    Update akka-actor, ... to 2.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
     val Twirl            = "1.5.0-M5" // sync with docs/project/plugins.sbt
     val PlayFileWatch    = "1.1.8"
 
-    val Akka: String = sys.props.getOrElse("lagom.build.akka.version", "2.6.0-RC2") // sync with docs/build.sbt
+    val Akka: String = sys.props.getOrElse("lagom.build.akka.version", "2.6.0") // sync with docs/build.sbt
     val AkkaHttp     = "10.1.10"
 
     val AkkaPersistenceCassandra = "0.100"
