7033317239470ca66aa1b8fe11d82591eeb4d8bc
Dale Wijnand <dale.wijnand@gmail.com>
2019-11-20 18:04:39 +0000

    [1.4] Fix CassandraClusteredPersistentEntitySpec
    Manually fixed cherry-pick of #2460.

--- persistence/scaladsl/src/test/scala/com/lightbend/lagom/scaladsl/persistence/multinode/AbstractClusteredPersistentEntitySpec.scala
+++ persistence/scaladsl/src/test/scala/com/lightbend/lagom/scaladsl/persistence/multinode/AbstractClusteredPersistentEntitySpec.scala
@@ -15,6 +15,7 @@ import akka.remote.testconductor.RoleName
 import akka.remote.testkit.MultiNodeConfig
 import akka.remote.testkit.MultiNodeSpec
 import akka.testkit.ImplicitSender
+import akka.util.Timeout
 import com.lightbend.lagom.scaladsl.persistence._
 import com.lightbend.lagom.scaladsl.playjson.JsonSerializerRegistry
 import com.typesafe.config.Config
@@ -46,20 +47,34 @@ abstract class AbstractClusteredPersistentEntityConfig extends MultiNodeConfig {
       terminate-system-after-member-removed = 60s
 
       # increase default timeouts to leave wider margin for Travis.
-      # 30s to 60s
-      akka.testconductor.barrier-timeout=60s
-      # 5s to 9s
-      lagom.persistence.ask-timeout = 9s
-      # 5s to 11s
-      akka.test.single-expect-default = 11s
-      ## use 9s and 11s for the above timeout because it's coprime values and it'll be easier to spot interferences.
-      ## Also, make the Akka expect() timeouts higher since this tests often expect over an ask operation.
+      akka.testconductor.barrier-timeout=90s
+
+      ## use coprime values for the timeouts below because it'll be easier to spot interferences.
+      ## Also, make Akka's `single-expect-default` timeout higher since this test often `expect`'s over an ask operation.
+      ## NOTE: these values used to be '9s' and '11s' but '9s' triggered timeouts quite often in Travis. If '13s'
+      ## also triggers timeouts in Travis it's possible there's something worth reviewing on this test.
+      lagom.persistence.ask-timeout = 13s
+      akka.test.single-expect-default = 15s
+      lagom.persistence.read-side.offset-timeout = 17s
 
       # Don't terminate the actor system when doing a coordinated shutdown
       # See http://doc.akka.io/docs/akka/2.5.0/project/migration-guide-2.4.x-2.5.x.html#Coordinated_Shutdown
       akka.coordinated-shutdown.terminate-actor-system = off
       akka.coordinated-shutdown.run-by-jvm-shutdown-hook = off
       akka.cluster.run-coordinated-shutdown-when-down = off
+
+      # multi-jvm tests forms the cluster programmatically
+      # therefore we disable Akka Cluster Bootstrap
+      lagom.cluster.bootstrap.enabled = off
+
+      # no jvm exit on tests
+      lagom.cluster.exit-jvm-when-system-terminated = off
+
+      akka.cluster.sharding.waiting-for-state-timeout = 5s
+
+      # make sure ensure active kicks in fast enough on tests
+      lagom.persistence.cluster.distribution.ensure-active-interval = 2s
+
     """
         )
         .withFallback(ConfigFactory.parseResources("play/reference-overrides.conf"))
@@ -112,13 +127,15 @@ abstract class AbstractClusteredPersistentEntitySpec(config: AbstractClusteredPe
   // implicit EC needed for pipeTo
   import system.dispatcher
 
+  final def dilatedTimeout = testConductor.Settings.BarrierTimeout.duration
+
   override def initialParticipants = roles.size
 
   def join(from: RoleName, to: RoleName): Unit = {
     runOn(from) {
       Cluster(system).join(node(to).address)
     }
-    enterBarrier(from.name + "-joined")
+    enterBarrierWithDilatedTimeout(from.name + "-joined")
   }
 
   def fullAddress(ref: ActorRef): Address =
@@ -136,7 +153,7 @@ abstract class AbstractClusteredPersistentEntitySpec(config: AbstractClusteredPe
       awaitAssert(Cluster(system).state.members.map(_.status) should be(Set(MemberStatus.Up)))
     }
 
-    enterBarrier("startup")
+    enterBarrierWithDilatedTimeout("startup")
   }
 
   def components: PersistenceComponents
@@ -162,32 +179,40 @@ abstract class AbstractClusteredPersistentEntitySpec(config: AbstractClusteredPe
     }
   }
 
+  def enterBarrierWithDilatedTimeout(name: String) = {
+    import akka.testkit._
+    testConductor.enter(
+      Timeout.durationToTimeout(remainingOr(testConductor.Settings.BarrierTimeout.duration.dilated)),
+      scala.collection.immutable.Seq(name)
+    )
+  }
+
   "A PersistentEntity in a Cluster" must {
 
     "send commands to target entity" in within(75.seconds) {
       // this barrier at the beginning of the test will be run on all nodes and should be at the
       // beginning of the test to ensure it's run.
-      enterBarrier("before-1")
+      enterBarrierWithDilatedTimeout("start 'send commands to target entity'")
 
-      val ref1 = registry.refFor[TestEntity]("1")
-      val ref2 = registry.refFor[TestEntity]("2")
+      val ref1 = registry.refFor[TestEntity]("entity-1")
+      val ref2 = registry.refFor[TestEntity]("entity-2")
 
       // STEP 1: send some commands from all nodes of the test to ref1 and ref2
       // note that this is done on node1, node2 and node 3 !!
       val r1 = ref1.ask(TestEntity.Add("a"))
       r1.pipeTo(testActor)
       expectMsg(TestEntity.Appended("A"))
-      enterBarrier("appended-A")
+      enterBarrierWithDilatedTimeout("appended-A")
 
       val r2 = ref2.ask(TestEntity.Add("b"))
       r2.pipeTo(testActor)
       expectMsg(TestEntity.Appended("B"))
-      enterBarrier("appended-B")
+      enterBarrierWithDilatedTimeout("appended-B")
 
       val r3: Future[TestEntity.Evt] = ref2.ask(TestEntity.Add("c"))
       r3.pipeTo(testActor)
       expectMsg(TestEntity.Appended("C"))
-      enterBarrier("appended-C")
+      enterBarrierWithDilatedTimeout("appended-C")
 
       // STEP 2: assert both ref's stored all the commands in their respective state.
       val r4: Future[TestEntity.State] = ref1.ask(TestEntity.Get)
@@ -201,15 +226,16 @@ abstract class AbstractClusteredPersistentEntitySpec(config: AbstractClusteredPe
 
       // STEP 3: assert the number of events consumed in the read-side processors equals the number of expected events.
       // NOTE: in nodes node2 and node3 {{expectAppendCount}} is a noop
-      expectAppendCount("1", 3)
-      expectAppendCount("2", 6)
+      expectAppendCount("entity-1", 3)
+      expectAppendCount("entity-2", 6)
 
+      enterBarrierWithDilatedTimeout("end 'send commands to target entity'")
     }
 
     "run entities on specific node roles" in {
       // this barrier at the beginning of the test will be run on all nodes and should be at the
       // beginning of the test to ensure it's run.
-      enterBarrier("before-2")
+      enterBarrierWithDilatedTimeout("start 'run entities on specific node roles'")
       // node1 and node2 are configured with "backend" role
       // and lagom.persistence.run-entities-on-role = backend
       // i.e. no entities on node3
@@ -222,27 +248,29 @@ abstract class AbstractClusteredPersistentEntitySpec(config: AbstractClusteredPe
         expectMsgType[Address]
       }.toSet
 
-      addresses should not contain (node(node3).address)
+      addresses should not contain node(node3).address
+
+      enterBarrierWithDilatedTimeout("end 'run entities on specific node roles'")
     }
 
     "have support for graceful leaving" in {
       // this barrier at the beginning of the test will be run on all nodes and should be at the
       // beginning of the test to ensure it's run.
-      enterBarrier("before-3")
+      enterBarrierWithDilatedTimeout("start 'have support for graceful leaving'")
 
       runOn(node2) {
         Await.ready(registry.gracefulShutdown(20.seconds), 20.seconds)
       }
-      enterBarrier("node2-left")
+      enterBarrierWithDilatedTimeout("node2-left")
 
       runOn(node1) {
         within(35.seconds) {
-          val ref1                       = registry.refFor[TestEntity]("1")
+          val ref1                       = registry.refFor[TestEntity]("entity-1")
           val r1: Future[TestEntity.Evt] = ref1.ask(TestEntity.Add("a"))
           r1.pipeTo(testActor)
           expectMsg(TestEntity.Appended("A"))
 
-          val ref2                       = registry.refFor[TestEntity]("2")
+          val ref2                       = registry.refFor[TestEntity]("entity-2")
           val r2: Future[TestEntity.Evt] = ref2.ask(TestEntity.Add("b"))
           r2.pipeTo(testActor)
           expectMsg(TestEntity.Appended("B"))
@@ -253,7 +281,7 @@ abstract class AbstractClusteredPersistentEntitySpec(config: AbstractClusteredPe
         }
       }
 
-      enterBarrier("node1-working")
+      enterBarrierWithDilatedTimeout("end 'have support for graceful leaving'")
     }
   }
 }
