97e4a582974b9b1d9d24e293e5cc5e9c46883d13
Scala Steward <me@scala-steward.org>
2019-12-12 09:25:57 +0100

    Update play-json to 2.8.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
 
     // If you update the version of Play, you probably need to update the other Play* variables.
     val Play             = "2.8.0-RC5" // sync with docs/build.sbt
-    val PlayJson         = "2.8.0"
+    val PlayJson         = "2.8.1"
     val PlayStandaloneWs = "2.1.1"
     val Twirl            = "1.5.0" // sync with docs/project/plugins.sbt
     val PlayFileWatch    = "1.1.9"
