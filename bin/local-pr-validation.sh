c41242a7d46ac5c7ea3d92689d2e70f7cfda55db
Dale Wijnand <dale.wijnand@gmail.com>
2020-01-03 09:55:02 +0000

    build: Switch to using javafmtCheckAll

--- bin/local-pr-validation.sh
+++ bin/local-pr-validation.sh
@@ -8,19 +8,13 @@
 printMessage "VALIDATE FRAMEWORK CODE"
 sbt +headerCheck +test:headerCheck multi-jvm:headerCheck \
   scalafmtAll scalafmtSbt \
-  javafmt test:javafmt multi-jvm:javafmt
+  javafmtCheckAll
 
 printMessage "VALIDATE DOCS CODE"
 pushd docs
 sbt headerCheck test:headerCheck \
   scalafmtAll scalafmtSbt \
-  javafmt test:javafmt
+  javafmtCheckAll
 popd
-
-git diff --exit-code || (
-  echo "WARN: Code changed after format and license headers validation. See diff above."
-  echo "You need to commit the new changes or amend the existing commit."
-  false
-)
 
 printMessage "ALL VALIDATIONS DONE"
