a159d75f5cc24b7524b30fea53380b4fddcb7fdd
Marcos Pereira <marcos.silva@gmail.com>
2020-01-30 11:42:22 -0500

    Update documentation links

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -49,9 +49,9 @@ object Dependencies {
     val Netty                = "4.1.45.Final"
     val NettyReactiveStreams = "2.0.4"
     // adapt links in (java/scala)/KafkaClient.md for minor version changes
-    val AlpakkaKafka  = "2.0.1"
+    val AlpakkaKafka = "2.0.1"
     // Keep this version consistent with Alpakka Kafka Connector
-    val Kafka         = "2.4.0"
+    val Kafka = "2.4.0"
 
     val Curator       = "2.12.0"
     val Immutables    = "2.8.3"
