9198d73e46144c44db727f9f9760883ba56c41c2
Ignasi Marimon-Clos <ignasi@lightbend.com>
2020-02-21 15:29:06 +0100

    Introduce Projections Spi

--- persistence/scaladsl/src/main/scala/com/lightbend/lagom/internal/scaladsl/persistence/AbstractPersistentEntityRegistry.scala
+++ persistence/scaladsl/src/main/scala/com/lightbend/lagom/internal/scaladsl/persistence/AbstractPersistentEntityRegistry.scala
@@ -4,28 +4,25 @@
 
 package com.lightbend.lagom.internal.scaladsl.persistence
 
-import java.util.Optional
-import java.util.concurrent.CompletionStage
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.TimeUnit
 
 import akka.actor.ActorSystem
-import akka.actor.CoordinatedShutdown
 import akka.cluster.Cluster
 import akka.cluster.sharding.ClusterSharding
 import akka.cluster.sharding.ClusterShardingSettings
 import akka.cluster.sharding.ShardRegion
 import akka.event.Logging
-import akka.pattern.ask
 import akka.persistence.query.Offset
 import akka.persistence.query.PersistenceQuery
 import akka.persistence.query.scaladsl.EventsByTagQuery
 import akka.stream.scaladsl
-import akka.util.Timeout
-import akka.Done
 import akka.NotUsed
+import akka.annotation.InternalStableApi
+import akka.persistence.query.EventEnvelope
+import com.lightbend.lagom.internal.spi.projections.ProjectionsSpi
 import com.lightbend.lagom.scaladsl.persistence._
-import scala.concurrent.Future
+
 import scala.concurrent.duration._
 import scala.reflect.ClassTag
 
@@ -135,6 +132,7 @@ class AbstractPersistentEntityRegistry(system: ActorSystem) extends PersistentEn
 
         queries
           .eventsByTag(tag, fromOffset)
+          .map(envelope => ProjectionsSpi.startProcessing(tag, envelope))
           .map(env =>
             new EventStreamElement[Event](
               PersistentEntityActor.extractEntityId(env.persistenceId),
