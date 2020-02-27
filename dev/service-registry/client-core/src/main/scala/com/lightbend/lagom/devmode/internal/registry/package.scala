79bf0da64002785bfeec81593727076102659a21
Guy Youansi <ralphlaude@yahoo.fr>
2020-02-27 19:08:03 +0100

    355-rename-lagom-dev-mode-components: add package

--- dev/service-registry/client-core/src/main/scala/com/lightbend/lagom/devmode/internal/registry/package.scala
+++ dev/service-registry/client-core/src/main/scala/com/lightbend/lagom/devmode/internal/registry/package.scala
@@ -2,7 +2,7 @@
  * Copyright (C) Lightbend Inc. <https://www.lightbend.com>
  */
 
-package com.lightbend.lagom.internal
+package com.lightbend.lagom.devmode.internal.registry
 
 import java.net.URI
 
