97aa4970608952887fc5bb6824ee833e54e4387f
Sergey Morgunov <hostage@me.com>
2020-04-02 20:34:37 +0300

    #2508 Upgrade to Alpakka Kafka 2.0

--- dev/build-tool-support/src/main/scala/com/lightbend/lagom/dev/StaticServiceLocations.scala
+++ dev/build-tool-support/src/main/scala/com/lightbend/lagom/dev/StaticServiceLocations.scala
@@ -7,7 +7,7 @@ package com.lightbend.lagom.dev
 private[lagom] object StaticServiceLocations {
   def staticServiceLocations(lagomCassandraPort: Int, lagomKafkaAddress: String): Map[String, String] = {
     Map(
-      "cas_native"   -> s"tcp://127.0.0.1:$lagomCassandraPort/cas_native"
+      "cas_native" -> s"tcp://127.0.0.1:$lagomCassandraPort/cas_native"
     )
   }
 }
