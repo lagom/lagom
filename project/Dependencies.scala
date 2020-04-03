27efc82f0409141f89e52e0237fa4f397af2b45b
Scala Steward <me@scala-steward.org>
2020-04-04 00:19:58 +0200

    Update asm, asm-analysis, asm-commons, ... to 8.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -187,7 +187,7 @@ object Dependencies {
     "org.jetbrains"        % "annotations"          % "13.0"
   )
 
-  val ow2asmDeps = libraryFamily("org.ow2.asm", "7.3.1")(
+  val ow2asmDeps = libraryFamily("org.ow2.asm", "8.0.1")(
     "asm",
     "asm-analysis",
     "asm-commons",
