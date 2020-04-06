42aa8464b214d9ab25aec405e7a3686e65c53ea9
Renato Cavalcanti <renato@cavalcanti.be>
2020-04-06 17:40:40 +0200

    Start CassandraLauncher before ActorSystem (always)

--- persistence-cassandra/scaladsl/src/test/scala/com/lightbend/lagom/scaladsl/persistence/cassandra/CassandraPersistenceSpec.scala
+++ persistence-cassandra/scaladsl/src/test/scala/com/lightbend/lagom/scaladsl/persistence/cassandra/CassandraPersistenceSpec.scala
@@ -6,9 +6,9 @@ package com.lightbend.lagom.scaladsl.persistence.cassandra
 
 import java.io.File
 
-import akka.actor.setup.ActorSystemSetup
 import akka.actor.ActorSystem
 import akka.actor.BootstrapSetup
+import akka.actor.setup.ActorSystemSetup
 import akka.cluster.Cluster
 import akka.persistence.cassandra.testkit.CassandraLauncher
 import com.lightbend.lagom.internal.persistence.testkit.AwaitPersistenceInit.awaitPersistenceInit
@@ -19,20 +19,32 @@ import com.lightbend.lagom.scaladsl.playjson.JsonSerializerRegistry
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 
-class CassandraPersistenceSpec private (system: ActorSystem) extends ActorSystemSpec(system) {
-  def this(testName: String, config: Config, jsonSerializerRegistry: JsonSerializerRegistry) =
+abstract class CassandraPersistenceSpec private (actorSystemFactory: () => ActorSystem)
+    extends ActorSystemSpec(actorSystemFactory) {
+  def this(testName: String, config: Config, jsonSerializerRegistry: JsonSerializerRegistry) = {
+
     this(
-      ActorSystem(
-        testName,
-        ActorSystemSetup(
-          BootstrapSetup(
-            config
-              .withFallback(cassandraConfig(testName, CassandraLauncher.randomPort))
-          ),
-          JsonSerializerRegistry.serializationSetupFor(jsonSerializerRegistry)
+      () => {
+        // first start Cassandra and bind the necessary ports
+        val cassandraDirectory = new File("target/" + testName)
+        CassandraLauncher.start(cassandraDirectory, "lagom-test-embedded-cassandra.yaml", clean = true, port = 0)
+
+        // start the ActorSystem
+        // note that we first need to bind the Cassandra port and then pass it to the ActorSystem config
+        // this is needed to allow the Cassandra plugin to connected to the randomly selected port
+        ActorSystem(
+          testName,
+          ActorSystemSetup(
+            BootstrapSetup(
+              config
+                .withFallback(cassandraConfig(testName, CassandraLauncher.randomPort))
+            ),
+            JsonSerializerRegistry.serializationSetupFor(jsonSerializerRegistry)
+          )
         )
-      )
+      }
     )
+  }
 
   def this(config: Config, jsonSerializerRegistry: JsonSerializerRegistry) =
     this(PersistenceSpec.testNameFromCallStack(classOf[CassandraPersistenceSpec]), config, jsonSerializerRegistry)
@@ -41,11 +53,7 @@ class CassandraPersistenceSpec private (system: ActorSystem) extends ActorSystem
 
   override def beforeAll(): Unit = {
     super.beforeAll()
-
-    val cassandraDirectory = new File("target/" + system.name)
-    CassandraLauncher.start(cassandraDirectory, "lagom-test-embedded-cassandra.yaml", clean = true, port = 0)
     awaitPersistenceInit(system)
-
     // Join ourselves - needed because the Cassandra offset store uses cluster startup task
     val cluster = Cluster(system)
     cluster.join(cluster.selfAddress)
