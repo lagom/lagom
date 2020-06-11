28a9fec716abc404d35fa040d9e75e368d03dd0f
Scala Steward <me@scala-steward.org>
2020-06-11 16:32:20 +0200

    Update akka-persistence-cassandra, ... to 1.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -31,7 +31,7 @@ object Dependencies {
     val Akka: String = sys.props.getOrElse("lagom.build.akka.version", "2.6.4") // sync with docs/build.sbt
     val AkkaHttp     = "10.1.12"
 
-    val AkkaPersistenceCassandra = "0.103"
+    val AkkaPersistenceCassandra = "1.0.1"
     // this is the version used in AkkaPersistenceCassandra, we stick with it
     val CassandraDriver     = "3.7.2"
     val AkkaPersistenceJdbc = "3.5.3"
