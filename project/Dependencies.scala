32abf033cc976b57166f2d8107386548ec5a9763
Scala Steward <me@scala-steward.org>
2020-08-01 01:34:31 +0200

    Update byte-buddy to 1.10.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -77,7 +77,7 @@ object Dependencies {
     val jetty = "9.4.20.v20190813"
 
     val Selenium  = "3.141.59"
-    val ByteBuddy = "1.10.13"
+    val ByteBuddy = "1.10.14"
   }
 
   // Some setup before we start creating ModuleID vals
