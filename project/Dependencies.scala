48dee0f18f8b4c142d174e39df01e514b6218736
Scala Steward <me@scala-steward.org>
2020-12-13 16:36:12 +0100

    jffi 1.3.1 (was 1.2.23)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -202,7 +202,7 @@ object Dependencies {
     "asm-util"
   )
 
-  private val jffi         = "com.github.jnr" % "jffi"          % "1.2.23"
+  private val jffi         = "com.github.jnr" % "jffi"          % "1.3.1"
   private val jnrConstants = "com.github.jnr" % "jnr-constants" % "0.9.15"
   private val jnrFfi       = "com.github.jnr" % "jnr-ffi"       % "2.1.15"
   private val jnrPosix     = "com.github.jnr" % "jnr-posix"     % "3.0.61"
