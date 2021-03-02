36f4082cec8ef30db9eebb3654876d8e826b9bb4
Renato Cavalcanti <renato@cavalcanti.be>
2021-03-02 17:20:09 +0100

    update scala version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,8 +12,8 @@ object Dependencies {
   object Versions {
     // Version numbers
 
-    val Scala212 = "2.12.10"
-    val Scala213 = "2.13.1"
+    val Scala212 = "2.12.13"
+    val Scala213 = "2.13.5"
     val Scala    = Seq(Scala212, Scala213)
     val SbtScala = Seq(Scala212)
 
