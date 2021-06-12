583d67455cd5ee6da084d41a3ae2ffee16f5fc1a
Scala Steward <me@scala-steward.org>
2021-06-12 04:24:26 +0200

    scala-java8-compat 1.0.0 (was 0.9.1)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -61,7 +61,7 @@ object Dependencies {
     val HibernateCore = "5.4.30.Final"
     val PCollections  = "3.1.4"
 
-    val ScalaJava8Compat = "0.9.1"
+    val ScalaJava8Compat = "1.0.0"
     val ScalaXml         = "1.3.0"
     val Slick            = "3.3.2"
     // JUnit[Interface] should be sync with:
