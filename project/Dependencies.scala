ee2f6c1acfb940efe28075b6bd33fc804ca0e9d3
Renato Cavalcanti <renato@cavalcanti.be>
2021-12-03 10:37:38 +0100

    update javassit - remove jdk11 warning

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -95,7 +95,7 @@ object Dependencies {
   private val scalaJava8Compat       = "org.scala-lang.modules" %% "scala-java8-compat" % Versions.ScalaJava8Compat
   private val scalaCollectionCompat  = "org.scala-lang.modules" %% "scala-collection-compat" % "2.1.2"
   private val scalaXml               = "org.scala-lang.modules" %% "scala-xml" % Versions.ScalaXml
-  private val javassist              = "org.javassist" % "javassist" % "3.24.0-GA"
+  private val javassist              = "org.javassist" % "javassist" % "3.28.0-GA"
   private val byteBuddy              = "net.bytebuddy" % "byte-buddy" % Versions.ByteBuddy
   private val scalaParserCombinators = "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2"
   private val typesafeConfig         = "com.typesafe" % "config" % "1.4.0"
